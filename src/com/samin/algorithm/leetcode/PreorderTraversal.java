12a8b058a307fc4064a375d9c5207d82e8eba7b8
samin <822085977@qq.com>
2020-09-23 14:13:35 +0800

    feature: Add preorder traversal and remove duplicates leetcode problems

---
+++ src/com/samin/algorithm/leetcode/PreorderTraversal.java
@@ -0,0 +1,36 @@
+package com.samin.algorithm.leetcode;
+
+import com.samin.algorithm.base.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/** 二叉树的前序遍历 */
+public class PreorderTraversal {
+
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+
+        list.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+
+        t1.right = t2;
+        t2.left = t3;
+
+        System.out.println(new PreorderTraversal().preorderTraversal(t1));
+    }
+}
