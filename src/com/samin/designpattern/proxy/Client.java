44fb871a79be9872e5b3a9a811bb66904318bd93
samin <822085977@qq.com>
2020-09-27 11:29:24 +0800

    docs: Modify the annotation

--- src/com/samin/designpattern/proxy/Client.java
+++ src/com/samin/designpattern/proxy/Client.java
@@ -1,8 +1,11 @@
 package com.samin.designpattern.proxy;
 
-/*
+/**
  * 代理模式
- * */
+ *
+ * 代理者和被代理者必须实现同一个接口
+ * 代理者的目的是隐藏被代理者的行为
+ */
 public class Client {
 
     public static void main(String[] args) {
--- src/com/samin/designpattern/strategy/Client.java
+++ src/com/samin/designpattern/strategy/Client.java
@@ -1,8 +1,10 @@
 package com.samin.designpattern.strategy;
 
-/*
+/**
  * 策略模式
- * */
+ *
+ * 通过多态，让使用者感知不到内部策略的变化，策略方法本身是主体
+ */
 public class Client {
 
     public static void main(String[] args) {
