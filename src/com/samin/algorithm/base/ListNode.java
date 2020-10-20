71f24e8121b84fee046aebc544555bbb975b8348
samin <822085977@qq.com>
2020-10-20 19:50:02 +0800

    feature: Add algorithm reorder list

--- src/com/samin/algorithm/base/ListNode.java
+++ src/com/samin/algorithm/base/ListNode.java
@@ -14,4 +14,33 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    public static ListNode listNodeGenerator(int[] arrs) {
+        ListNode head = null;
+        ListNode current = null;
+        int index = 0;
+
+        while (index < arrs.length) {
+            if (head == null) {
+                head = new ListNode(arrs[index]);
+                current = head;
+            } else {
+                current.next = new ListNode(arrs[index]);
+                current = current.next;
+            }
+
+            index = index + 1;
+        }
+
+        return head;
+    }
+
+    public static void listNodePrinter(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+
+        System.out.println("");
+    }
 }
---
+++ src/com/samin/algorithm/leetcode/ReorderList.java
@@ -0,0 +1,48 @@
+package com.samin.algorithm.leetcode;
+
+import com.samin.algorithm.base.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReorderList {
+
+    public void reorderList(ListNode head) {
+        ListNode tmp = head;
+
+        // 获取所有的值
+        List<Integer> vals = new ArrayList<>();
+
+        while (head != null) {
+            vals.add(head.val);
+            head = head.next;
+        }
+
+        // 重新排序
+        head = tmp;
+        int left = 0;
+        int right = vals.size() - 1;
+        for (int i = 0; i < vals.size(); i++) {
+            if (i % 2 == 0) {
+                head.val = vals.get(left++);
+            } else {
+                head.val = vals.get(right--);
+            }
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = ListNode.listNodeGenerator(new int[] {1, 2, 3, 4});
+        new ReorderList().reorderList(l1);
+        ListNode.listNodePrinter(l1);
+
+        ListNode l2 = ListNode.listNodeGenerator(new int[] {1, 2, 3, 4, 5});
+        new ReorderList().reorderList(l2);
+        ListNode.listNodePrinter(l2);
+
+        ListNode l3 = ListNode.listNodeGenerator(new int[] {});
+        new ReorderList().reorderList(l3);
+        ListNode.listNodePrinter(l3);
+    }
+}
