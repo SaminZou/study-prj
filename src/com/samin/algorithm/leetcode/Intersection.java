cb7227f6403da9c6e888d9b3f360d590d28bc47a
samin <822085977@qq.com>
2020-11-02 23:13:18 +0800

    feature: Add algorithm

--- src/com/samin/algorithm/leetcode/Intersection.java
+++ src/com/samin/algorithm/leetcode/Intersection.java
@@ -1,37 +1,28 @@
 package com.samin.algorithm.leetcode;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Intersection {
 
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        for (Integer ele : nums1) {
-            set1.add(ele);
-        }
+        // int[] to List<Integer>
+        List<Integer> list1 = Arrays.stream(nums1).boxed().collect(Collectors.toList());
+        List<Integer> list2 = Arrays.stream(nums2).boxed().collect(Collectors.toList());
 
-        Set<Integer> set2 = new HashSet<>();
-        for (Integer ele : nums2) {
-            set2.add(ele);
-        }
+        // do intersection
+        list1.retainAll(list2);
 
-        set1 =
-                set1.stream()
-                        .sorted(Comparator.naturalOrder())
-                        .collect(Collectors.toCollection(LinkedHashSet::new));
-        set2 =
-                set2.stream()
-                        .sorted(Comparator.naturalOrder())
-                        .collect(Collectors.toCollection(LinkedHashSet::new));
+        // delete the same number
+        Set<Integer> resSet = new HashSet<>(list1);
 
-        int[] res = new int[Math.max(set1.size(), set2.size())];
-
-        return null;
+        // return result
+        return new ArrayList<>(resSet).stream().mapToInt(Integer::valueOf).toArray();
     }
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        // [2] 、 [9，4]
+        System.out.println(Arrays.toString(new Intersection().intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+        System.out.println(Arrays.toString(new Intersection().intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})));
+    }
 }
