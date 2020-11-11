c056a9197f8cfd035ec65e97eda64cb50ead7d30
samin <822085977@qq.com>
2020-11-11 18:32:04 +0800

    feature: Add algorithm

---
+++ src/com/samin/algorithm/leetcode/BalancedStringSplit.java
@@ -0,0 +1,39 @@
+package com.samin.algorithm.leetcode;
+
+import java.util.Stack;
+
+// 分割平衡字符串
+public class BalancedStringSplit {
+
+    public int balancedStringSplit(String s) {
+        int res = 0;
+
+        Stack<Character> temp = new Stack<>();
+        for (Character ele : s.toCharArray()) {
+            // 空的情况直接插入元素
+            if (temp.size() == 0) {
+                temp.push(ele);
+            } else {
+                // 不配对，入栈
+                if (ele.equals(temp.peek())) {
+                    temp.push(ele);
+                    // 配对出栈，并判断栈是否为空，为空证明有相同数量的平衡字符串
+                } else {
+                    temp.pop();
+                    if (temp.size() == 0) {
+                        res += 1;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        // 4 3 1
+        System.out.println(new BalancedStringSplit().balancedStringSplit("RLRRLLRLRL"));
+        System.out.println(new BalancedStringSplit().balancedStringSplit("RLLLLRRRLR"));
+        System.out.println(new BalancedStringSplit().balancedStringSplit("LLLLRRRR"));
+    }
+}
