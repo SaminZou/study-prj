0a1543d15861e0fc80f2a4b29bf68a76fb9b5b8d
samin <822085977@qq.com>
2020-11-28 22:41:09 +0800

    feature: Add algorithm

---
+++ src/com/samin/algorithm/leetcode/RestoreString.java
@@ -0,0 +1,31 @@
+package com.samin.algorithm.leetcode;
+
+// 重新排列字符串
+public class RestoreString {
+
+    public String restoreString(String s, int[] indices) {
+        // 结果
+        char[] res = new char[s.length()];
+
+        // 重新排序
+        int index = 0;
+        for (int ele : indices) {
+            res[ele] = s.toCharArray()[index++];
+        }
+
+        return new String(res);
+    }
+
+    public static void main(String[] args) {
+        // leetcode
+        System.out.println(new RestoreString().restoreString("codeleet", new int[]{4, 5, 6, 7, 0, 2, 1, 3}));
+        // abc
+        System.out.println(new RestoreString().restoreString("abc", new int[]{0, 1, 2}));
+        // nihao
+        System.out.println(new RestoreString().restoreString("aiohn", new int[]{3, 1, 4, 2, 0}));
+        // arigatou
+        System.out.println(new RestoreString().restoreString("aaiougrt", new int[]{4, 0, 2, 6, 7, 3, 1, 5}));
+        // rat
+        System.out.println(new RestoreString().restoreString("art", new int[]{1, 0, 2}));
+    }
+}
