aabd6d638f2162cc3ee5d3b5e26032cedcfe6d8b
SaminZou <822085977@qq.com>
2021-06-24 10:51:37 +0800

    ✨ Feat: add coding

---
+++ leetcode/src/algorithms/StrStr.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+/**
+ * 实现 strStr()
+ *
+ * @author samin
+ * @date 2021-06-24
+ */
+public class StrStr {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new StrStr().strStr("hello","ll"));
+        System.out.println(new StrStr().strStr("hello",""));
+    }
+}
