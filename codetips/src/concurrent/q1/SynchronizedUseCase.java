88b1cfbeb0c4ed3862ca0da270faeec716f71c2c
SaminZou <822085977@qq.com>
2021-08-23 16:40:41 +0800

    ✨ Feat: enhance coding

--- codetips/src/concurrent/q1/SynchronizedUseCase.java
+++ codetips/src/concurrent/q1/SynchronizedUseCase.java
@@ -8,11 +8,15 @@ import java.util.concurrent.TimeUnit;
 /**
  * Synchronized 用例
  *
+ * <p> synchronized关键字用于加锁，但这种锁一是很重，二是获取时必须一直等待，没有额外的尝试机制
+ *
  * @author samin
  * @date 2021-01-08
  */
 public class SynchronizedUseCase {
-    public static int sums = 0;
+
+    public static int sumsWithLock = 0;
+    public static int sumsWithoutLock = 0;
 
     public static void main(String[] args) throws Exception {
         ThreadPoolExecutor poolExecutor =
@@ -24,26 +28,42 @@ public class SynchronizedUseCase {
                         new LinkedBlockingQueue<>(10),
                         (ThreadFactory) Thread::new);
 
+        // 未加锁
         for (int i = 0; i < 5; i++) {
             poolExecutor.execute(
                     () -> {
                         for (int j = 0; j < 5000; j++) {
-                            // 加锁控制
+                            sumsWithoutLock += 1;
+                        }
+                    });
+        }
+
+        // 加锁
+        for (int i = 0; i < 5; i++) {
+            poolExecutor.execute(
+                    () -> {
+                        for (int j = 0; j < 5000; j++) {
+                            // lock
                             synchronized (SynchronizedUseCase.class) {
-                                sums += 1;
+                                sumsWithLock += 1;
                             }
                         }
                     });
         }
 
-        // 等待全部线程执行完毕，结果观测才是有意义的
+        // 等待全部线程执行完毕，结果观测才是有意义的，睡眠 5 秒待结果处理完成
         Thread.sleep(5000);
         poolExecutor.shutdown();
 
-        System.out.println("sums: " + sums);
+        // 注意：未加锁的测试结果可能正确，多执行几次观测结果
+        System.out.println("【未加锁模块测试结果】sumsWithoutLock: " + sumsWithoutLock);
+        System.out.println("【加锁模块测试结果】sumsUseLock: " + sumsWithLock);
         System.out.println("---------------------------------------");
     }
 
+    /**
+     * 加锁实例
+     */
     public void method() {
         synchronized (this) {
             System.out.println("synchoronized 代码块");
--- codetips/src/concurrent/q5/ReentrantLockUseCase.java
+++ codetips/src/concurrent/q5/ReentrantLockUseCase.java
@@ -1,12 +1,84 @@
 package concurrent.q5;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * ReentrantLock 用例
+ *
+ * <p> 使用ReentrantLock比直接使用synchronized更安全，线程在tryLock()失败的时候不会导致死锁。
+ *
+ * <p> ReentrantLock可以替代synchronized进行同步；
+ *
+ * <p> ReentrantLock获取锁更安全；
+ *
+ * <p> 必须先获取到锁，再进入try {...}代码块，最后使用finally保证释放锁；
+ *
+ * <p> 可以使用tryLock()尝试获取锁。
  *
  * @author samin
  * @date 2021-01-08
  */
 public class ReentrantLockUseCase {
 
-    public static void main(String[] args) {}
+    public static int sumsWithLock = 0;
+    public static int sumsWithoutLock = 0;
+
+    public static void main(String[] args) throws Exception {
+        ThreadPoolExecutor poolExecutor =
+                new ThreadPoolExecutor(
+                        5,
+                        10,
+                        10,
+                        TimeUnit.MINUTES,
+                        new LinkedBlockingQueue<>(10),
+                        (ThreadFactory) Thread::new);
+
+        // 未加锁
+        for (int i = 0; i < 5; i++) {
+            poolExecutor.execute(
+                    () -> {
+                        for (int j = 0; j < 5000; j++) {
+                            sumsWithoutLock += 1;
+                        }
+                    });
+        }
+
+        // 加锁
+        for (int i = 0; i < 5; i++) {
+            poolExecutor.execute(
+                    () -> {
+                        final Lock lock = new ReentrantLock();
+                        for (int j = 0; j < 5000; j++) {
+                            // lock
+                            lock.lock();
+                            try {
+                                // 在尝试获取锁的时候，最多等待1秒。如果1秒后仍未获取到锁，tryLock()返回false，程序就可以做一些额外处理，而不是无限等待下去。
+                                if (lock.tryLock(1, TimeUnit.SECONDS)) {
+                                    try {
+                                        sumsWithLock += 1;
+                                    } finally {
+                                        lock.unlock();
+                                    }
+                                }
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+        }
+
+        // 等待全部线程执行完毕，结果观测才是有意义的，睡眠 5 秒待结果处理完成
+        Thread.sleep(5000);
+        poolExecutor.shutdown();
+
+        // 注意：未加锁的测试结果可能正确，多执行几次观测结果
+        System.out.println("【未加锁模块测试结果】sumsWithoutLock: " + sumsWithoutLock);
+        System.out.println("【加锁模块测试结果】sumsUseLock: " + sumsWithLock);
+        System.out.println("---------------------------------------");
+    }
 }
