55a02d3a1d0e450fdd540d5aa30a3a204497054a
SaminZou <822085977@qq.com>
2022-01-21 17:36:38 +0800

    Feat: enhance coding

--- codetips/src/basic/q10/DateSortUseCase.java
+++ codetips/src/basic/q10/DateSortUseCase.java
@@ -6,7 +6,9 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 时间列表排序
@@ -17,9 +19,9 @@ import java.util.List;
 public class DateSortUseCase {
 
     public static void main(String[] args) {
-        String date1 = "2018-10-22 10:11:12";
-        String date2 = "2021-10-22 11:11:12";
-        String date3 = "2021-10-22 09:11:12";
+        String date1 = "2018-10-21 10:11:12";
+        String date2 = "2021-10-23 11:11:12";
+        String date3 = "2021-10-20 09:11:12";
         String date4 = "2020-10-22 12:11:12";
 
         List<SortObj> list = new ArrayList<>();
@@ -42,6 +44,21 @@ public class DateSortUseCase {
         // 2 3 4 1
         Collections.sort(list);
         System.out.println(list);
+
+        // 时间排序
+        List<OffsetDateTime> dateList = new ArrayList<>();
+        dateList.add(OffsetDateTime.of(LocalDateTime.parse(date1, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                ZoneOffset.of(DateTimeUseCase.OFFSET_ID)));
+        dateList.add(OffsetDateTime.of(LocalDateTime.parse(date2, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                ZoneOffset.of(DateTimeUseCase.OFFSET_ID)));
+        dateList.add(OffsetDateTime.of(LocalDateTime.parse(date3, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                ZoneOffset.of(DateTimeUseCase.OFFSET_ID)));
+        dateList.add(OffsetDateTime.of(LocalDateTime.parse(date4, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                ZoneOffset.of(DateTimeUseCase.OFFSET_ID)));
+
+        System.out.println(dateList);
+
+        System.out.println(dateList.stream().sorted(Comparator.comparing(e -> e)).collect(Collectors.toList()));
     }
 
     private static class SortObj implements Comparable<SortObj> {
@@ -62,11 +79,7 @@ public class DateSortUseCase {
 
         @Override
         public String toString() {
-            return "ObjWithDate{" +
-                    "foo=" + foo +
-                    ", bar='" + bar + '\'' +
-                    ", time=" + time +
-                    '}' + "\n";
+            return "ObjWithDate{" + "foo=" + foo + ", bar='" + bar + '\'' + ", time=" + time + '}' + "\n";
         }
 
         @Override
