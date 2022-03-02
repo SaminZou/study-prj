8f7cf0a8dc9fafe9a174aa421bd2d09646a1dab5
SaminZou <822085977@qq.com>
2022-03-02 19:39:50 +0800

    ✨ Feat: enhance doc

--- codetips/src/concurrent/q4/ScheduledThreadPoolExecutorUseCase.java
+++ codetips/src/concurrent/q4/ScheduledThreadPoolExecutorUseCase.java
@@ -1,5 +1,8 @@
 package concurrent.q4;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 相比 ThreadPoolExecutor，ScheduledThreadPoolExecutor 可以在给定的延迟后运行命令，或者定期执行命令
  *
@@ -9,6 +12,20 @@ package concurrent.q4;
 public class ScheduledThreadPoolExecutorUseCase {
 
     public static void main(String[] args) {
-        
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+        // 1 秒后开始执行任务，每隔 2 秒执行一次
+        executor.scheduleWithFixedDelay(new DelayTask(), 1000, 2000, TimeUnit.MICROSECONDS);
+    }
+
+    private static class DelayTask implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("task start ...");
+
+            // doBusiness();
+
+            System.out.println("task finish ...");
+        }
     }
 }
