fe7a2deb0aaa86bffb15bb711606faecaef7799e
SaminZou <822085977@qq.com>
2022-03-06 15:54:02 +0800

    ✨ Feat: enhance doc

--- codetips/src/basic/q6/chat/Client.java
+++ codetips/src/basic/q6/chat/Client.java
@@ -1,6 +1,10 @@
 package basic.q6.chat;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 /**
@@ -21,14 +25,16 @@ public class Client {
         Client.clientRun();
     }
 
-    /** 简单双工聊天，开启后客户端先发送信息 */
+    /**
+     * 简单双工聊天，开启后客户端先发送信息
+     */
     public static void clientRun() throws IOException {
         Socket s = new Socket("127.0.0.1", 8081);
         DataInputStream din = new DataInputStream(s.getInputStream());
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = "", str2 = "";
+        String str = "", str2;
         while (!"stop".equals(str)) {
             str = br.readLine();
             dout.writeUTF(str);
