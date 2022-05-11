888bd759bfb5c26201285199843c9b9736d96c99
SaminZou <822085977@qq.com>
2022-05-11 11:01:51 +0800

    ✨ Feat: enhance blog

--- leetcode/src/algorithms/MaxSubArray.java
+++ leetcode/src/algorithms/MaxSubArray.java
@@ -17,10 +17,11 @@ package algorithms;
 public class MaxSubArray {
 
     public static void main(String[] args) {
-        System.out.println(new MaxSubArray().maxSubArray(new int[] {}));
+        System.out.println(new MaxSubArray().maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
     public int maxSubArray(int[] nums) {
+        // 特殊入参
         if (nums.length == 0) {
             return 0;
         }
@@ -29,6 +30,7 @@ public class MaxSubArray {
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
+            // 只要前面的结果是负数，就从下一个开始重新累加（隐藏原则，负数不管加负数还是加正数，一定比正数从新累加小）
             if (dp[i - 1] > 0) {
                 dp[i] = dp[i - 1] + nums[i];
             } else {
@@ -36,6 +38,25 @@ public class MaxSubArray {
             }
 
             res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+
+    // 使用原数组存储，不需要额外空间
+    public int maxSubArray2(int[] nums) {
+        // 特殊入参
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // 只要前面的结果是负数，就从下一个开始重新累加（隐藏原则，负数不管加负数还是加正数，一定比正数从新累加小）
+            if (nums[i - 1] > 0) {
+                nums[i] = nums[i - 1] + nums[i];
+            }
+            res = Math.max(res, nums[i]);
         }
 
         return res;
