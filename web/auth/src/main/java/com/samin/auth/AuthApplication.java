8adcea8c6aa48e1a41ae1d5dd1e76d46ce17a95d
samin <822085977@qq.com>
2022-08-07 00:12:52 +0800

    ✨ Feat: enhance web coding

--- web/auth/src/main/java/com/samin/auth/AuthApplication.java
+++ web/auth/src/main/java/com/samin/auth/AuthApplication.java
@@ -9,5 +9,4 @@ public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
     }
-
 }
--- web/auth/src/main/java/com/samin/auth/config/HttpSecurityConfig.java
+++ web/auth/src/main/java/com/samin/auth/config/HttpSecurityConfig.java
@@ -1,14 +1,21 @@
 package com.samin.auth.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Spring Boot Security 配置类
+ *
+ * @author samin
+ * @date 2022-08-07
+ */
+@Slf4j
 @Configuration
 public class HttpSecurityConfig extends WebSecurityConfigurerAdapter  {
-
 
     @Override
     public void configure(WebSecurity web) {
