2de64d1a26bb7552ed8abca6b07b57a186bdfe5c
SaminZou <822085977@qq.com>
2022-09-28 12:11:50 +0800

    ✨ Feat: enhance coding

--- codetips/src/basic/q8/PassByValueUseCase.java
+++ codetips/src/basic/q8/PassByValueUseCase.java
@@ -37,10 +37,10 @@ public class PassByValueUseCase {
         User user2 = new User();
         user2.name = "original string";
         System.out.println("call function before: " + user2.name);
-        // 看第二个实例调用第二个方法，如果还是引用传递的话，user2的值应该会变，实际上并没有
+        // 看第二个实例调用第二个方法，如果还是引用传递的话，user2 的值应该会变，实际上并没有
         System.out.println("call function after: " + user2.name);
 
-        // 总结，值传递的本质为实参进入方法后是否复制了副本给形参，原始类型和String类型很明显是复制了副本
+        // 总结，值传递的本质为实参进入方法后是否复制了副本给形参，原始类型和 String 类型很明显是复制了副本
         // 对于对象而言，其实也复制了一个副本，只不过是指向了同一个地址，修改副本指向地址内容，当然实参也会变化，但是修改副本的地址，实参不变
         // 对象的传递其实是 `按共享传递`，本质是特殊的值传递
     }
--- codetips/src/basic/q9/KeyboardInput.java
+++ codetips/src/basic/q9/KeyboardInput.java
@@ -18,18 +18,23 @@ public class KeyboardInput {
         Scanner input = new Scanner(System.in);
         String s = input.nextLine();
         input.close();
+        System.out.println("1: "+s);
 
         // 方法2
         BufferedReader brInput = new BufferedReader(new InputStreamReader(System.in));
         String s2 = brInput.readLine();
+        System.out.println("2: "+s2);
     }
 
-    //    public static void main(String[] args) throws IOException {
-    //        while (true) {
-    //            Scanner input = new Scanner(System.in);
-    //            String s = input.nextLine();
-    //            Integer num = Integer.parseInt(s);
-    //            System.out.println("现在完成：" + num / 240 + "圈");
-    //        }
-    //    }
+    /**
+     * 交互式
+     */
+    // public static void main(String[] args) throws IOException {
+    //     while (true) {
+    //         Scanner input = new Scanner(System.in);
+    //         String s = input.nextLine();
+    //         Integer num = Integer.parseInt(s);
+    //         System.out.println("现在完成：" + num / 240 + "圈");
+    //     }
+    // }
 }
