afa80417fc35ecd6b0d5bddd5ccd881a8d71fdec
samin <822085977@qq.com>
2022-10-08 00:04:19 +0800

    ✨ Feat: enhance coding

--- codetips/src/basic/q1/SystemCallUseCase.java
+++ codetips/src/basic/q1/SystemCallUseCase.java
@@ -13,12 +13,17 @@ public class SystemCallUseCase {
     public static void main(String[] args) {
         // Arrays.copyOf() 底层调用的就是 System.arraycopy()
 
-        // 模拟从arr1复制从2开头的3个数字到arr2;
+        // System.arraycopy() 入参分别是源数组，源数组开头，目的数组，目的数组开头，复制长度
         int[] arr1 = new int[]{1, 2, 3, 4, 5};
         int[] arr2 = new int[3];
-        // 入参分别是源数组，源数组开头，目的数组，目的数组开头，复制长度
+        int[] arr3 = new int[arr1.length + 1];
+        // 模拟从 arr1 复制从 2 开头的 3 个数字到 arr2
         System.arraycopy(arr1, 1, arr2, 0, 3);
         System.out.println(Arrays.toString(arr2));
+        // 模拟从 arr1 复制全部数字到 arr3
+        System.arraycopy(arr1, 0, arr3, 0, arr1.length);
+        arr3[arr3.length - 1] = 6;
+        System.out.println(Arrays.toString(arr3));
 
         // identityHashCode() 获取对象的系统和内部地址相关的 hashCode()
         // 可用于判断是否是同一个实例对象
