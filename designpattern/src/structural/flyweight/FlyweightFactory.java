c607a977ed8cbe9e462e6f99e107e818070d9185
SaminZou <822085977@qq.com>
2022-10-12 10:37:06 +0800

    ✨ Feat: enhance design pattern

--- designpattern/src/structural/flyweight/FlyweightFactory.java
+++ designpattern/src/structural/flyweight/FlyweightFactory.java
@@ -2,6 +2,7 @@ package structural.flyweight;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class FlyweightFactory {
 
@@ -9,8 +10,8 @@ public class FlyweightFactory {
 
     public static Shape getShape(String key) {
         Shape shape = shapes.get(key);
-        // 如果shape==null,表示不存在,则新建,并且保持到共享池中
-        if (shape == null) {
+        // 如果 shape 为空，表示不存在，则新建，并且保持到共享池中
+        if (Objects.isNull(shape)) {
             shape = new Circle(key);
             shapes.put(key, shape);
         }
