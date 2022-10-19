0b02d060b56036474706e5db35013ae1efc11880
SaminZou <822085977@qq.com>
2022-10-19 13:02:18 +0800

    ✨ Feat: enhance algorithms

--- leetcode/src/algorithms/CanBeEqual.java
+++ leetcode/src/algorithms/CanBeEqual.java
@@ -5,7 +5,7 @@ import algorithms.base.util.QuickSort;
 /**
  * 通过翻转子数组使两个数组相等
  *
- * <p>最好的方式，使用桶排序
+ * <p> 最好的方式，使用桶排序
  *
  * @author samin
  * @date 2021-01-11
@@ -13,11 +13,18 @@ import algorithms.base.util.QuickSort;
 public class CanBeEqual {
 
     public static void main(String[] args) {
-        System.out.println(new CanBeEqual().canBeEqual(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3}));
-        System.out.println(new CanBeEqual().canBeEqual(new int[]{7}, new int[]{7}));
-        System.out.println(new CanBeEqual().canBeEqual(new int[]{1, 12}, new int[]{12, 1}));
-        System.out.println(new CanBeEqual().canBeEqual(new int[]{3, 7, 9}, new int[]{3, 7, 11}));
-        System.out.println(new CanBeEqual().canBeEqual(new int[]{1, 1, 1, 1, 1}, new int[]{1, 1, 1, 1, 1}));
+        // true
+        System.out.println(new CanBeEqual().bucket(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3}));
+        // true
+        System.out.println(new CanBeEqual().bucket(new int[]{7}, new int[]{7}));
+        // true
+        System.out.println(new CanBeEqual().bucket(new int[]{1, 12}, new int[]{12, 1}));
+        // false
+        System.out.println(new CanBeEqual().bucket(new int[]{3, 7, 9}, new int[]{3, 7, 11}));
+        // true
+        System.out.println(new CanBeEqual().bucket(new int[]{1, 1, 1, 1, 1}, new int[]{1, 1, 1, 1, 1}));
+        // false
+        System.out.println(new CanBeEqual().bucket(new int[]{1, 2, 2, 3}, new int[]{1, 1, 2, 3}));
     }
 
     public boolean canBeEqual(int[] target, int[] arr) {
@@ -26,6 +33,26 @@ public class CanBeEqual {
 
         for (int i = 0; i < target.length; i++) {
             if (target[i] != arr[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean bucket(int[] target, int[] arr) {
+        if (target.length != arr.length) {
+            return false;
+        }
+
+        int[] bucket = new int[1001];
+        for (int i = 0; i < target.length; i++) {
+            bucket[target[i]] += 1;
+            bucket[arr[i]] -= 1;
+        }
+
+        for (int i = 0; i < bucket.length; i++) {
+            if (bucket[i] != 0) {
                 return false;
             }
         }
