7e5878711791cb30729d35297c63f59453fbcaf6
SaminZou <822085977@qq.com>
2022-10-27 17:37:32 +0800

    ✨ Feat: add web coding

---
+++ web/use-case-demo/usecase/src/main/java/com/samin/usecase/UsecaseApplication.java
@@ -0,0 +1,13 @@
+package com.samin.usecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsecaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsecaseApplication.class, args);
+    }
+
+}
