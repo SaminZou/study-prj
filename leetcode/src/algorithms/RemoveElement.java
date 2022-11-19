872a5eac7e65d915e301e0f0edeafca61d764dbf
SaminZou <822085977@qq.com>
2022-11-19 15:10:43 +0800

    ✨ Feat: enhance algorithms

--- leetcode/src/algorithms/RemoveElement.java
+++ leetcode/src/algorithms/RemoveElement.java
@@ -1,15 +1,42 @@
 package algorithms;
 
+import java.util.Arrays;
+
 /**
- * 移除元素
+ * 移除元素 https://leetcode-cn.com/problems/remove-element/
  *
  * @author samin
  * @date 2021-01-11
  */
 public class RemoveElement {
 
-    // TODO https://leetcode-cn.com/problems/remove-element/
+    public static void main(String[] args) {
+        // 2,[2,2,...]
+        int[] arr1 = new int[]{3, 2, 2, 3};
+        System.out.println(new RemoveElement().removeElement(arr1, 3));
+        System.out.println(Arrays.toString(arr1));
+
+        // 5,[0,1,4,0,3,...]
+        int[] arr2 = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(new RemoveElement().removeElement(arr2, 2));
+        System.out.println(Arrays.toString(arr2));
+    }
+
     public int removeElement(int[] nums, int val) {
-        return 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        // 索引
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[result] = nums[i];
+                result += 1;
+            }
+        }
+
+        return result;
     }
 }
