f3f76430843f654a56f06589bd4de6a54a53c962
SaminZou <822085977@qq.com>
2022-12-07 17:41:24 +0800

    ✨ Feat: enhance web coding

--- web/prometheus-demo/README.md
+++ web/prometheus-demo/README.md
@@ -3,3 +3,11 @@ The main steps to setting up alerting and notifications are:
 1. Setup and configure the Alertmanager
 2. Configure Prometheus to talk to the Alertmanager
 3. Create alerting rules in Prometheus
+
+# 数据可视化分类
+
+- 变化趋势 Graph
+
+- 分布统计 Heatmap
+
+- 当前状态 singleStat
--- web/prometheus-demo/src/main/java/com/example/prometheus/controller/TestController.java
+++ web/prometheus-demo/src/main/java/com/example/prometheus/controller/TestController.java
@@ -7,6 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试控制类
+ *
+ * @author samin
+ * @date 2022-12-07
+ */
 @Slf4j
 @RestController
 public class TestController {
