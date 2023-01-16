793b17310677c5fb2b5d744157cbf838edc6459b
SaminZou <822085977@qq.com>
2023-01-16 16:08:12 +0800

    ✨ Feat: enhance web coding

--- web/demo/swagger-demo/src/main/java/com/samin/swaggerdemo/config/SwaggerConfig.java
+++ web/demo/swagger-demo/src/main/java/com/samin/swaggerdemo/config/SwaggerConfig.java
@@ -2,7 +2,9 @@ package com.samin.swaggerdemo.config;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,29 +12,31 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2
 @EnableKnife4j
+@ComponentScan({"springfox.documentation.schema"})
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
-                .apiInfo(apiInfo())
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .enable(true)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.samin.swaggerdemo.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.samin.swaggerdemo"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
 
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().description("接口文档")
+        return new ApiInfoBuilder().title("API测试文档")
+                .description("接口文档")
                 .contact(new Contact("samin", "https://github.com/SaminZou", "822085977@qq.com"))
                 .version("v1.0.0")
-                .title("API测试文档")
                 .build();
     }
 }
