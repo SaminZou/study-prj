6925855afbe2e3bc73214e5e22d0b6331d8afaa7
SaminZou <822085977@qq.com>
2023-01-29 09:43:29 +0800

    ✨ Feat: enhance web coding

--- web/demo/use-case-demo/src/main/java/com/samin/usecase/UseCaseApplication.java
+++ web/demo/use-case-demo/src/main/java/com/samin/usecase/UseCaseApplication.java
@@ -2,7 +2,9 @@ package com.samin.usecase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class UseCaseApplication {
 
