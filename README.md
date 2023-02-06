243435104411dab2de6c39ea80c2af97d0363f28
SaminZou <822085977@qq.com>
2023-02-06 12:32:44 +0800

    ✨ Feat: add project tool

--- README.md
+++ README.md
@@ -75,3 +75,9 @@ Java Web Demo
 # 6 License
 
 This project is licensed under the Apache License 2.0 - see the [LICENSE.txt](https://github.com/SaminZou/study-prj/blob/master/LICENSE.txt) file for details
+
+# FAQ
+
+Q：idea 导入后，可能出现异常需要重新导入工程项目，可以使用 `clean-idea-project-files.py` 删除工程文件
+
+$ python clean-idea-project-files.py
---
+++ clean-idea-project-files.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+
+class CleanTool:
+    @staticmethod
+    def clean_dir(path, dirName):
+        for root, dirs, files in os.walk(path):
+            for name in dirs:
+                if name == dirName:
+                    print("[clean directory] : " + os.path.join(root, name))
+                    shutil.rmtree(os.path.join(root, name))
+
+    @staticmethod
+    def clean_file(path, fileSuffix):
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                if name.endswith(fileSuffix):
+                    print("[clean file] : " + os.path.join(root, name))
+                    os.remove(os.path.join(root, name))
+
+
+if __name__ == "__main__":
+    dir_path = os.getcwd()
+    print("[current work directory] : " + dir_path)
+    CleanTool.clean_dir(dir_path, ".idea")
+    CleanTool.clean_file(dir_path, ".iml")
