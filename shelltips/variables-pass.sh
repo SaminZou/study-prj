7b0004b83a34c70bd58697aa5c2e45892510c790
SaminZou <822085977@qq.com>
2023-02-17 16:31:27 +0800

    ✨ Feat: add shell tips

---
+++ shelltips/variables-pass.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 方法一、二，二选一运行
+
+# 方法一，设置 set -o nounset
+set -o nounset
+set -o errexit
+
+# 方法二，没有设置 set -o nounset 的前提下使用以下语句
+#if [ ! -n "$1" ] ; then
+#    echo "you must input a variable !!!"
+#    exit
+#fi
+
+echo "variable is: $1"
