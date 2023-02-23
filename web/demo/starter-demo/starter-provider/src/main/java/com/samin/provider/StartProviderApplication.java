8d9130f40fa078c9dda20ebba8fa02fc18990b51
SaminZou <822085977@qq.com>
2023-02-23 11:25:04 +0800

    ✨ Feat: add web coding

---
+++ web/demo/starter-demo/starter-provider/src/main/java/com/samin/provider/StartProviderApplication.java
@@ -0,0 +1,12 @@
+package com.samin.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartProviderApplication.class, args);
+    }
+}
---
+++ web/demo/starter-demo/starter-worker/src/main/java/com/samin/worker/StarterWorkerApplication.java
@@ -0,0 +1,12 @@
+package com.samin.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterWorkerApplication.class, args);
+    }
+}
