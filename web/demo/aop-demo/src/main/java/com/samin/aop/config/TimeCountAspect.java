f16bced9e17c67a2d9f13c775b19b4a4a814d6c5
SaminZou <822085977@qq.com>
2023-03-09 22:16:04 +0800

    ⚡️ Pref: enhance web coding

---
+++ web/demo/aop-demo/src/main/java/com/samin/aop/config/TimeCountAspect.java
@@ -0,0 +1,47 @@
+package com.samin.aop.config;
+
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Slf4j
+@Aspect
+@Component
+public class TimeCountAspect {
+
+    // 统计请求的处理时间
+    ThreadLocal<Long> startTime = new ThreadLocal<>();
+
+    /**
+     * 带有 @TimeCount 注解的方法
+     */
+    @Pointcut("@annotation(com.samin.aop.annotation.TimeCount)")
+    public void timeCount() {
+
+    }
+
+    @Before("timeCount()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        startTime.set(System.currentTimeMillis());
+        //接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        //记录请求的内容
+        log.info("请求URL: {}", request.getRequestURL().toString());
+        log.info("请求METHOD: {}", request.getMethod());
+    }
+
+    @AfterReturning(pointcut = "timeCount()")
+    public void doAfterReturning() {
+        //处理完请求后，返回内容
+        log.info("方法执行时间: {}", (System.currentTimeMillis() - startTime.get()));
+        startTime.remove();
+    }
+}
--- web/demo/aop-demo/src/main/java/com/samin/aop/controller/TestController.java
+++ web/demo/aop-demo/src/main/java/com/samin/aop/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.samin.aop.controller;
 
+import com.samin.aop.annotation.TimeCount;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,13 @@ public class TestController {
 
     @GetMapping(value = "/test")
     public String test(@RequestParam(value = "param") String param) {
+        return "success";
+    }
+
+    @TimeCount
+    @GetMapping(value = "/timeCount")
+    public String timeCount() throws InterruptedException {
+        Thread.sleep((long) (Math.random() * 4 + 1) * 1000L);
         return "success";
     }
 }
