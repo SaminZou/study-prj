f0ad9987fcc5d1f84c8b633ad49a4246e8af19d4
SaminZou <822085977@qq.com>
2023-03-15 19:34:26 +0800

    ✨ Feat: add algorithms coding

--- leetcode/src/algorithms/FindRepeatNumber.java
+++ leetcode/src/algorithms/FindRepeatNumber.java
@@ -1,15 +1,26 @@
 package algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 数组中重复的数字
+ *
+ * <p> https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
  *
  * @author samin
  * @date 2021-01-11
  */
 public class FindRepeatNumber {
 
-    // TODO https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
     public int findRepeatNumber(int[] nums) {
-        return 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return num;
+            }
+            map.put(num, 1);
+        }
+        return -1;
     }
 }
