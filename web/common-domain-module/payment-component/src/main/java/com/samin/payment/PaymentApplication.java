da52b64210502ec4ad3a07a753d994676ca88b03
SaminZou <822085977@qq.com>
2023-03-20 12:39:37 +0800

    ✨ Feat: add common-domain-module

---
+++ web/common-domain-module/payment-component/src/main/java/com/samin/payment/PaymentApplication.java
@@ -0,0 +1,12 @@
+package com.samin.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
