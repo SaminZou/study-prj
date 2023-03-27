21277b32629b0bbc4af8e43a5338d081ea1f9fcb
SaminZou <822085977@qq.com>
2023-03-27 17:44:27 +0800

    ✨ Feat: add algorithms coding

--- leetcode/src/algorithms/SubtractProductAndSum.java
+++ leetcode/src/algorithms/SubtractProductAndSum.java
@@ -3,13 +3,31 @@ package algorithms;
 /**
  * 整数的各位积和之差
  *
+ * <p> https://leetcode-cn.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+ *
  * @author samin
  * @date 2021-01-11
  */
 public class SubtractProductAndSum {
 
-    // TODO https://leetcode-cn.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+    public static void main(String[] args) {
+        // 15
+        System.out.println(new SubtractProductAndSum().subtractProductAndSum(234));
+        // 21
+        System.out.println(new SubtractProductAndSum().subtractProductAndSum(4421));
+    }
+
     public int subtractProductAndSum(int n) {
-        return 0;
+        // sum：和；difference：差；product：积；quotient：商
+        int sum = 0;
+        int product = 1;
+
+        while (n > 0) {
+            sum += n % 10;
+            product *= n % 10;
+            n = n / 10;
+        }
+
+        return product - sum;
     }
 }
