d36812b145d2de9effa3ace995af80b0da3f760f
SaminZou <822085977@qq.com>
2023-03-28 14:34:27 +0800

    ✨ Feat: add algorithms coding

--- leetcode/src/algorithms/NumJewelsInStones.java
+++ leetcode/src/algorithms/NumJewelsInStones.java
@@ -3,13 +3,33 @@ package algorithms;
 /**
  * 宝石与石头
  *
+ * <p> https://leetcode-cn.com/problems/jewels-and-stones/
+ *
  * @author samin
  * @date 2021-01-11
  */
 public class NumJewelsInStones {
 
-    // TODO https://leetcode-cn.com/problems/jewels-and-stones/
+    public static void main(String[] args) {
+        // 3
+        System.out.println(new NumJewelsInStones().numJewelsInStones("aA", "aAAbbbb"));
+        // 0
+        System.out.println(new NumJewelsInStones().numJewelsInStones("z", "ZZ"));
+    }
+
     public int numJewelsInStones(String jewels, String stones) {
-        return 0;
+        int result = 0;
+        char[] jewelsArr = jewels.toCharArray();
+
+        for (char stone : stones.toCharArray()) {
+            for (char jewelsChar : jewelsArr) {
+                if (stone == jewelsChar) {
+                    result += 1;
+                    break;
+                }
+            }
+        }
+
+        return result;
     }
 }
