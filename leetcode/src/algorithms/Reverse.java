4086888fc9d31dba817e5c28171843ad96da8876
SaminZou <822085977@qq.com>
2023-03-29 14:55:58 +0800

    ✨ Feat: add algorithms code

--- leetcode/src/algorithms/Reverse.java
+++ leetcode/src/algorithms/Reverse.java
@@ -3,13 +3,46 @@ package algorithms;
 /**
  * 整数反转
  *
+ * <p> https://leetcode-cn.com/problems/reverse-integer/
+ *
  * @author samin
  * @date 2021-01-11
  */
 public class Reverse {
 
-    // TODO https://leetcode-cn.com/problems/reverse-integer/
+    public static void main(String[] args) {
+        // 321
+        System.out.println(new Reverse().reverse(123));
+        // -321
+        System.out.println(new Reverse().reverse(-123));
+        // 21
+        System.out.println(new Reverse().reverse(120));
+        // 0
+        System.out.println(new Reverse().reverse(0));
+        // -4321
+        System.out.println(new Reverse().reverse(-1234));
+        // 0
+        System.out.println(new Reverse().reverse(1534236469));
+        // 0
+        System.out.println(new Reverse().reverse(-1534236469));
+    }
+
     public int reverse(int x) {
-        return 0;
+        // 位运算优先级在加减之后
+        int max = (1 << 31) - 1;
+        int min = -1 << 31;
+        long result = 0;
+
+        int carry = 10;
+        while (x > 0 | -x > 0) {
+            result = (result * carry) + (x % 10);
+            x = x / 10;
+        }
+
+        if (result > max || result < min) {
+            return 0;
+        }
+
+        return (int) result;
     }
 }
