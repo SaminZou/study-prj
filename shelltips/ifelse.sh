107c9c2269bf7620a166997a679c3bdef56de118
SaminZou <822085977@qq.com>
2023-03-29 11:57:47 +0800

    ✨ Feat: enhance shell

--- shelltips/ifelse.sh
+++ shelltips/ifelse.sh
@@ -4,21 +4,25 @@ set -o nounset
 set -o errexit
 
 num=$1
-flag=1
+flag=0
 
 # 非逻辑
 if [ ! "$flag" -eq 1 ]; then
   echo "trigger not logic"
+  # 正常结束程序
+  exit 0
 fi
 
-if [ "$num" == 1 ]; then
+if [ "$num" -eq 1 ]; then
   echo "1"
 # 或逻辑
-elif [ "$num" == 2 ] || [ "$num" == 3 ]; then
+elif [ "$num" -eq 2 ] || [ "$num" -eq 3 ]; then
   echo "2"
 # 与逻辑
-elif [ "$num" == 4 ] && [ "$flag" -eq 1 ]; then
+elif [ "$num" -eq 4 ] && [ "$flag" -eq 1 ]; then
   echo "3"
+elif [ "$num" -lt 10 ]; then
+  echo "2"
 else
   echo "unknow"
 fi
