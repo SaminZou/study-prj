82ff667307cb7439aca0061fa6f9be928852a41f
SaminZou <822085977@qq.com>
2023-04-17 21:13:29 +0800

    ✨ Feat: add web coding

--- web/demo-module/redis-demo/src/main/java/com/samin/redis/repository/HolidayRepository.java
+++ web/demo-module/redis-demo/src/main/java/com/samin/redis/repository/HolidayRepository.java
@@ -11,4 +11,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface HolidayRepository extends JpaRepository<Holiday, Integer> {
 
+    Holiday findHolidayById(Integer id);
 }
--- web/demo-module/redis-demo/src/main/java/com/samin/redis/service/HolidayService.java
+++ web/demo-module/redis-demo/src/main/java/com/samin/redis/service/HolidayService.java
@@ -4,21 +4,22 @@ import com.samin.redis.entity.Holiday;
 import com.samin.redis.entity.HolidayStatsVo;
 import com.samin.redis.repository.HolidayRepository;
 import com.samin.redis.util.DateUtil;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.temporal.ChronoField;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 用户服务类
@@ -62,8 +63,120 @@ public class HolidayService {
     }
 
     @CacheEvict(cacheNames = "HOLIDAY_STATS", allEntries = true)
-    public Holiday save(Holiday holiday) {
-        // TODO 防止假期和工作日重复
+    public Holiday save(Holiday holiday) throws Exception {
+        // 检查假期和工作日配置是否重复
+        List<String> intersection = new ArrayList<>();
+        for (String item : holiday.getWeekdays()) {
+            if (ArrayUtils.contains(holiday.getHolidays(), item)) {
+                intersection.add(item);
+            }
+        }
+        if (!CollectionUtils.isEmpty(intersection)) {
+            throw new Exception("假期和补班设置不能重复");
+        }
+
+        // 防止假期和工作日重复
+        List<Holiday> all = holidayRepository.findAll();
+        // 已存在假期
+        List<String> holidays = getHolidays(all);
+        // 已存在补班
+        List<String> workDays = getWorkDays(all);
+
+        // 校验假期是否重复
+        if (holiday.getHolidays().length > 0) {
+            // 更新时，去掉校验历史设置参数
+            if (Objects.nonNull(holiday.getId())) {
+                Holiday historyHistory = holidayRepository.findHolidayById(holiday.getId());
+                if (Objects.nonNull(historyHistory) && historyHistory.getHolidays().length > 0) {
+                    holidays = holidays.stream()
+                            .filter(item -> !ArrayUtils.contains(historyHistory.getHolidays(), item))
+                            .collect(Collectors.toList());
+                }
+            }
+
+            // 校验假期和就假期间是否重复
+            intersection = new ArrayList<>();
+            for (String item : holiday.getHolidays()) {
+                if (holidays.contains(item)) {
+                    intersection.add(item);
+                }
+            }
+            if (!CollectionUtils.isEmpty(intersection)) {
+                throw new Exception("假期设置区间存在重复配置天数");
+            }
+
+            // 校验假期和就补班间是否重复
+            intersection = new ArrayList<>();
+            for (String item : holiday.getWeekdays()) {
+                if (holidays.contains(item)) {
+                    intersection.add(item);
+                }
+            }
+            if (!CollectionUtils.isEmpty(intersection)) {
+                throw new Exception("假期设置区间存在重复配置天数");
+            }
+
+            // 校验补班是否重复
+            if (holiday.getWeekdays().length > 0) {
+                // 更新时，去掉校验历史设置参数
+                if (Objects.nonNull(holiday.getId())) {
+                    Holiday historyHistory = holidayRepository.findHolidayById(holiday.getId());
+                    if (Objects.nonNull(historyHistory) && Objects.nonNull(historyHistory.getWeekdays())) {
+                        workDays = workDays.stream()
+                                .filter(item -> !ArrayUtils.contains(historyHistory.getWeekdays(), item))
+                                .collect(Collectors.toList());
+                    }
+                }
+
+                // 校验补班和补班间是否重复
+                intersection = new ArrayList<>();
+                for (String item : holiday.getWeekdays()) {
+                    if (workDays.contains(item)) {
+                        intersection.add(item);
+                    }
+                }
+                if (!CollectionUtils.isEmpty(intersection)) {
+                    throw new Exception("补班设置区间存在重复配置天数");
+                }
+
+                // 校验补班和假期是否重复
+                intersection = new ArrayList<>();
+                for (String item : holiday.getHolidays()) {
+                    if (workDays.contains(item)) {
+                        intersection.add(item);
+                    }
+                }
+                if (!CollectionUtils.isEmpty(intersection)) {
+                    throw new Exception("补班和假期存在重复配置天数");
+                }
+            }
+        } else {
+            // 校验补班是否重复
+            if (holiday.getWeekdays().length > 0) {
+                // 更新时，去掉校验历史设置参数
+                if (Objects.nonNull(holiday.getId())) {
+                    Holiday holidayHistory = holidayRepository.findHolidayById(holiday.getId());
+                    if (Objects.nonNull(holidayHistory) && Objects.nonNull(holidayHistory.getWeekdays())) {
+                        workDays = workDays.stream()
+                                .filter(item -> !ArrayUtils.contains(holidayHistory.getWeekdays(), item))
+                                .collect(Collectors.toList());
+                    }
+                }
+
+                // 校验补班和补班间是否重复
+                intersection = new ArrayList<>();
+                for (String item : holiday.getWeekdays()) {
+                    if (workDays.contains(item)) {
+                        intersection.add(item);
+                    }
+                }
+
+                if (!CollectionUtils.isEmpty(intersection)) {
+                    throw new Exception("补班设置区间存在重复配置天数");
+                }
+            }
+        }
+
         return holidayRepository.save(holiday);
     }
 
