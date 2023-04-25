fe2993976b79f2f424a2f997ae95ef23eb59e7b0
SaminZou <822085977@qq.com>
2023-04-25 14:44:09 +0800

    ✨ Feat: enhance shell tips

--- shelltips/variables-pass.sh
+++ shelltips/variables-pass.sh
@@ -9,12 +9,13 @@
 # 方法二，没有设置 set -o nounset 的前提下使用以下语句
 #if [ ! -n "$1" ] ; then
 #    echo "you must input a variable !!!"
-#    exit
+#    exit 0
 #fi
 
+# 定义为 0 的时候，程序是正确退出，exit 1 代表有错误，也可以通过这个来判断程序是否正常退出
 if [ -z $1 ]; then
   echo "you must input a variable"
-  exit
+  exit 0
 fi
 
 echo "variable is: $1"
