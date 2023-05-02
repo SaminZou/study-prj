908301fe3656a546d98aef4de0abb21b60f21fee
SaminZou <822085977@qq.com>
2023-05-02 10:08:19 +0800

    ✨ Feat: enhance shell tips

--- shelltips/grep-awk-sed/grep-awk-sed.sh
+++ shelltips/grep-awk-sed/grep-awk-sed.sh
@@ -14,7 +14,16 @@ grep -n "boo" foo_file
 echo "-v 为反向输出其他内容："
 grep -vn "boo" foo_file
 
-echo "符合筛选条件的数量："
-grep -c "boo" foo_file
+echo "以 'b' 开头的内容："
+grep "^b" foo_file
+
+echo "以 's' 结尾的内容："
+grep "s$" foo_file
+
+# awk
+# A text pattern scanning and processing language, created by Aho, Weinberger & Kernighan (hence the name).
+
+echo "计算当前目录所有文件的总大小："
+ls -l | awk 'BEGIN {sum=0} {sum=sum+$5} END {print sum}'
 
 # sed (stream editor)
