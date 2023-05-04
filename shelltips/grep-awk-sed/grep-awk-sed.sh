5d23598cc53d55d1cf569fb7a4d5b2f8f300668d
SaminZou <822085977@qq.com>
2023-05-04 13:57:53 +0800

    ✨ Feat: enhance shell tips

--- shelltips/grep-awk-sed/grep-awk-sed.sh
+++ shelltips/grep-awk-sed/grep-awk-sed.sh
@@ -28,15 +28,21 @@ ls -l | awk 'BEGIN {sum=0} {sum=sum+$5} END {print sum}'
 
 # sed (stream editor)
 
+# -e 和 -i 的区别，-e 是执行后标准输出，-i 会修改文件内容，即区别为是否会修改文件内容
+
 # sed -i ‘行数i\要插入的内容’
 # 注意，行数一定要是存在的，如文件只有两行内容，则 3i\111 不生效
 echo "在第二行插入 111："
 sed -i '2i\111' sed_test_file
 cat sed_test_file
+echo ''
 
 # sed -i '/DELETE STRING/d' * 删除所有文件中含有 “DELETE STRING” 行
 echo "删除 111："
 sed -i '/111/d' sed_test_file
 cat sed_test_file
+echo ''
 
 # sed 's/pattern/replace_string/g' file 替换文本
+echo "修改 input 为 output："
+sed -e 's/input/output/g' sed_test_file
