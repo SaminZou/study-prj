cd1034f1c210bb27023347b677c18d8e4ccaab8b
SaminZou <822085977@qq.com>
2023-05-07 21:26:35 +0800

    ✨ Feat: enhance code tips

--- codetips/src/concurrent/q1/ReentrantLockUseCase.java
+++ codetips/src/concurrent/q1/ReentrantLockUseCase.java
@@ -46,10 +46,10 @@ public class ReentrantLockUseCase {
             poolExecutor.execute(() -> {
                 final Lock lock = new ReentrantLock();
                 for (int j = 0; j < 5000; j++) {
-                    // lock
-                    lock.lock();
                     try {
-                        // 在尝试获取锁的时候，最多等待1秒。如果1秒后仍未获取到锁，tryLock() 返回 false，程序就可以做一些额外处理，而不是无限等待下去
+                        // lock
+                        lock.lock();
+                        // 在尝试获取锁的时候，最多等待 1 秒。如果 1 秒后仍未获取到锁，tryLock() 返回 false，程序就可以做一些额外处理，而不是无限等待下去
                         if (lock.tryLock(1, TimeUnit.SECONDS)) {
                             try {
                                 sumsWithLock += 1;
@@ -59,6 +59,8 @@ public class ReentrantLockUseCase {
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
+                    } finally {
+                        lock.unlock();
                     }
                 }
             });
