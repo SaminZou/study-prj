f8ed530a8ef7f3efc14a4cfd0214cdc9a1ae63e4
SaminZou <822085977@qq.com>
2023-05-09 18:01:55 +0800

    ✨ Feat: enhance web coding

--- web/demo-module/use-case-demo/src/main/java/com/samin/usecase/config/ConditionalConfig.java
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/config/ConditionalConfig.java
@@ -1,5 +1,6 @@
 package com.samin.usecase.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +11,14 @@ import org.springframework.context.annotation.Configuration;
  * @author samin
  * @date 2022-12-29
  */
+@Slf4j
 @Configuration
 @ConditionalOnProperty(value = {"custom.config.enabled"}, havingValue = "true", matchIfMissing = true)
 public class ConditionalConfig {
 
     @Bean
-    public String test(){
-        System.out.println("test String bean inject...");
+    public String test() {
+        log.info("[启动时加载] test String bean inject...");
         return "test";
     }
 }
