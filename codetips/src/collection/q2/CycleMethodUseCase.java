9e94e1c4a55e4f37a26e5ba4b31608e240ec7c6b
SaminZou <822085977@qq.com>
2023-05-17 21:41:34 +0800

    ✨ Feat: enhance code tips

--- codetips/src/collection/q2/CycleMethodUseCase.java
+++ codetips/src/collection/q2/CycleMethodUseCase.java
@@ -13,25 +13,28 @@ import java.util.LinkedList;
 public class CycleMethodUseCase {
 
     public static void main(String[] args) {
+        int listTestElementNum = 100000000;
+        int linkListTestElementNum = 100000;
+
         // 列表的循环
         ArrayList<Integer> list = new ArrayList<>();
         // index 循环
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < listTestElementNum; i++) {
             list.add(i);
         }
 
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < list.size(); i++) {
             Integer temp = list.get(i);
         }
-        System.out.println("list 1 time:" + (System.currentTimeMillis() - start));
+        System.out.println("list 1 index time:" + (System.currentTimeMillis() - start));
 
         // 增强型循环
         start = System.currentTimeMillis();
         for (Integer ele : list) {
             Integer temp = ele;
         }
-        System.out.println("list 2 time:" + (System.currentTimeMillis() - start));
+        System.out.println("list 2 enhance time:" + (System.currentTimeMillis() - start));
 
         // 迭代器循环
         start = System.currentTimeMillis();
@@ -39,27 +42,27 @@ public class CycleMethodUseCase {
         while (integerIterator.hasNext()) {
             Integer temp = integerIterator.next();
         }
-        System.out.println("list 3 time:" + (System.currentTimeMillis() - start));
+        System.out.println("list 3 iterator time:" + (System.currentTimeMillis() - start));
 
         // 链表的循环
         LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < linkListTestElementNum; i++) {
             linkedList.add(i);
         }
 
         // index 循环
         start = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < linkedList.size(); i++) {
             Integer temp = linkedList.get(i);
         }
-        System.out.println("linklist 1 time:" + (System.currentTimeMillis() - start));
+        System.out.println("linklist 1 index time:" + (System.currentTimeMillis() - start));
 
         // 增强型循环
         start = System.currentTimeMillis();
         for (Integer ele : linkedList) {
             Integer temp = ele;
         }
-        System.out.println("linklist 2 time:" + (System.currentTimeMillis() - start));
+        System.out.println("linklist 2 enhance time:" + (System.currentTimeMillis() - start));
 
         // 迭代器循环
         start = System.currentTimeMillis();
@@ -67,12 +70,12 @@ public class CycleMethodUseCase {
         while (integerIterator2.hasNext()) {
             Integer temp = integerIterator2.next();
         }
-        System.out.println("linklist 3 time:" + (System.currentTimeMillis() - start));
+        System.out.println("linklist 3 iterator time:" + (System.currentTimeMillis() - start));
 
         /*
         结论：
-        1. 列表的循环速度，index 循环 > 迭代器循环 > 增强型循环
-        2. 链表的循环速度，迭代器循环 > 增强型循环 > index 循环
+        1. 列表的循环速度，index 循环【优于】迭代器循环【优于】增强型循环
+        2. 链表的循环速度，迭代器循环【优于】增强型循环【优于】index 循环
         3. 链表的 index 循环速度非常糟糕
         4. 统一结论为，建议使用迭代器进行循环遍历，不管速度和数据安全性都比较高
          */
