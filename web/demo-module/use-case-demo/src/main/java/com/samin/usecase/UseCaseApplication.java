9280467ab3e9afa69b972f731c03a21d4903ab0d
SaminZou <822085977@qq.com>
2023-05-23 15:46:41 +0800

    ✨ Feat: enhance web coding

--- web/demo-module/use-case-demo/src/main/java/com/samin/usecase/UseCaseApplication.java
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/UseCaseApplication.java
@@ -1,14 +1,37 @@
 package com.samin.usecase;
 
+import com.samin.usecase.config.SystemProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
+@RequiredArgsConstructor
 @EnableScheduling
 @SpringBootApplication
-public class UseCaseApplication {
+public class UseCaseApplication implements CommandLineRunner {
+
+    private final SystemProperties systemProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(UseCaseApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        log.info("pgHost: {}", systemProperties.getPgHost());
+        log.info("pgPort: {}", systemProperties.getPgPort());
+        log.info("pgDatabase: {}", systemProperties.getPgDatabase());
+        log.info("pgUsername: {}", systemProperties.getPgUsername());
+        log.info("pgPassword: {}", systemProperties.getPgPassword());
+        log.info("pgSchema: {}", systemProperties.getPgSchema());
+        log.info("redisHost: {}", systemProperties.getRedisHost());
+        log.info("redisPort: {}", systemProperties.getRedisPort());
+        log.info("redisPassword: {}", systemProperties.getRedisPassword());
+        log.info("redisDatabase: {}", systemProperties.getRedisDatabase());
     }
 }
---
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/config/SystemProperties.java
@@ -0,0 +1,42 @@
+package com.samin.usecase.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 需要 setter 和 getter
+ *
+ * @author samin
+ * @date 2023-05-23
+ */
+@Setter
+@Getter
+@Component
+@ConfigurationProperties(prefix = "custom")
+public class SystemProperties {
+
+    /**
+     * 可以适配 pg_host / pg-host / pgHost
+     */
+    private String pgHost;
+
+    private String pgPort;
+
+    private String pgDatabase;
+
+    private String pgUsername;
+
+    private String pgPassword;
+
+    private String pgSchema;
+
+    private String redisHost;
+
+    private String redisPort;
+
+    private String redisPassword;
+
+    private String redisDatabase;
+}
