a64d4b349c3011b7e8ab2fffb9132068f40ed4f8
SaminZou <822085977@qq.com>
2023-06-07 16:04:14 +0800

    ✨ Feat: enhance shell tips

---
+++ shelltips/function-user-case/maven-project-deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# $1 项目路径 $2 过滤需要指定模块
+mavenProjectDeploy(){
+  cd $1
+  mvn clean compile package deploy -pl $2 -Dmaven.test.skip=true -DaltDeploymentRepository=<server_id>::default::http://127.0.0.1:8081/repository/maven-releases/
+}
+
+mavenProjectDeploy /d/study-prj/web/demo-module mq-demo/mq-common
