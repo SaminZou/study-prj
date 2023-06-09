c8e79b6f2dbfb96fd67cbb743a26e9ead353a19e
SaminZou <822085977@qq.com>
2023-06-09 16:31:25 +0800

    ✨ Feat: enhance leetcode

--- leetcode/src/algorithms/IsValid.java
+++ leetcode/src/algorithms/IsValid.java
@@ -27,6 +27,15 @@ public class IsValid {
         System.out.println(new IsValid().isValid("(])"));
     }
 
+    /**
+     * 思路：
+     * 1. 左括号全部入栈
+     * 2. 遇上右括号则开始和栈顶匹配
+     * 3. 最终能够全部抵消说明符合规则，反之栈的数量大于 0 说明存在不合规的括号对
+     *
+     * @param s
+     * @return
+     */
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
