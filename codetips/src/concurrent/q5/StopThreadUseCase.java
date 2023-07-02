97c3cf832e390a793d95e6e55ae734c994316924
SaminZou <822085977@qq.com>
2023-07-02 21:27:03 +0800

    ✨ Feat: enhance code tips

--- codetips/src/concurrent/q5/StopThreadUseCase.java
+++ codetips/src/concurrent/q5/StopThreadUseCase.java
@@ -1,5 +1,11 @@
 package concurrent.q5;
 
+import java.util.Random;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 停止一个正在运行的线程
  *
@@ -11,6 +17,7 @@ public class StopThreadUseCase {
     public static void main(String[] args) {
         // 停止正在运行的线程
         // stopThread();
+        // stopThreadV2();
         // 停止沉睡中的线程
         stopSleepThread();
     }
@@ -24,6 +31,27 @@ public class StopThreadUseCase {
             thread.interrupt();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * stopThread 方法的使用线程池的实现
+     */
+    static void stopThreadV2() {
+        TaskThread taskThread = new TaskThread();
+
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(9, 20, 10, TimeUnit.MINUTES, new LinkedBlockingQueue<>(100),
+                r -> new Thread(r, "show-the-error-thread-" + new Random().nextInt(999)));
+        Future<?> future = poolExecutor.submit(taskThread);
+
+        try {
+            Thread.sleep(100);
+
+            future.cancel(true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            poolExecutor.shutdown();
         }
     }
 
