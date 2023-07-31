5ef1464421ec30752be31fc4d999afbf27589c2a
SaminZou <822085977@qq.com>
2023-07-31 17:02:14 +0800

    ✨ Feat: enhance web coding

--- web/demo-module/auth-demo/src/main/java/com/samin/auth/AuthApplication.java
+++ web/demo-module/auth-demo/src/main/java/com/samin/auth/AuthApplication.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
+/**
+ * @author samin
+ * @Securitd，@RolesAllowed，@PreAuthorize，@PostAuthorize
+ * @Securitd，@RolesAllowed 和一样，是通用标准
+ * @PreAuthorize，@PostAuthorize 是 spring security 的定制注解，可以使用 SEL 表达式
+ * @date 2023-07-30
+ */
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @SpringBootApplication
 public class AuthApplication {
 
