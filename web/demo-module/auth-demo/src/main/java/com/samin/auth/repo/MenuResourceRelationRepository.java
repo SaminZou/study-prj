a16bd1c2c52a6f500c210e7629dbe089f34b8e8d
SaminZou <822085977@qq.com>
2023-09-10 21:17:36 +0800

    🚑 Quickfix: fix repository column mapping bug

--- web/demo-module/auth-demo/src/main/java/com/samin/auth/repo/MenuResourceRelationRepository.java
+++ web/demo-module/auth-demo/src/main/java/com/samin/auth/repo/MenuResourceRelationRepository.java
@@ -3,9 +3,6 @@ package com.samin.auth.repo;
 import com.samin.auth.entity.MenuResourceRelation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MenuResourceRelationRepository extends JpaRepository<MenuResourceRelation, Integer> {
 
-    List<MenuResourceRelation> findByMenuId(Integer menuId);
 }
