8343113997531e37814e2d3851b693fd2d92c27a
samin <822085977@qq.com>
2023-09-28 11:27:14 +0800

    ✨ Feat: enhance design pattern

--- designpattern/src/structural/bridge/Client.java
+++ designpattern/src/structural/bridge/Client.java
@@ -3,6 +3,8 @@ package structural.bridge;
 /**
  * 桥接模式
  *
+ * <p> 抽象是实现分离
+ *
  * @author samin
  * @date 2021-01-05
  */
@@ -17,9 +19,11 @@ public class Client {
         square.setColor(white);
         square.draw();
 
+        // 黑色
+        Black black = new Black();
         // 长方形
-        Shape rectange = new Rectangle();
-        rectange.setColor(white);
-        rectange.draw();
+        Shape rectangle = new Rectangle();
+        rectangle.setColor(black);
+        rectangle.draw();
     }
 }
