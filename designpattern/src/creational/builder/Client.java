d9d73ed313700cdc75b058fef4772641c3dfbec8
samin <822085977@qq.com>
2023-10-06 21:24:34 +0800

    ✨ Feat: enhance design pattern

--- designpattern/src/creational/builder/Client.java
+++ designpattern/src/creational/builder/Client.java
@@ -1,14 +1,14 @@
 package creational.builder;
 
-import creational.builder.style1.Builder;
-import creational.builder.style1.BuilderA;
-import creational.builder.style1.BuilderB;
-import creational.builder.style1.Director;
-import creational.builder.style1.Production;
+import creational.builder.style1.*;
 import creational.builder.style2.ProductionObj;
 
 /**
- * 建造者模式 Builder 接口或抽象类，非必选 ConcreateBuilder 具体的建造者，可以有多个 Product 普通的类，具体要被实例化的对象 Director 导演，统一指挥建造者建造目标，非必选
+ * 建造者模式
+ *
+ * <p> Builder 接口或抽象类，非必选 ConcreateBuilder 具体的建造者
+ *
+ * <p> 可以有多个 Product 普通的类，具体要被实例化的对象 Director 导演，统一指挥建造者建造目标，非必选
  *
  * <p>解决问题：参数多造成需要的构造器多，初始化分步设置属性容易出错
  */
@@ -35,7 +35,7 @@ public class Client {
         System.out.println(obj);
 
         // 可以指定特定元素值
-        obj = new ProductionObj.Builder("a", "d").b("bbb").c("ccc").build();
+        obj = new ProductionObj.Builder("a", "d").b("bbb").c("ccc").e("ee").build();
         System.out.println(obj);
 
         // style2 的使用方式比较实用，很容易理解此方法的用意，在 okhttp 工具包中有体现
