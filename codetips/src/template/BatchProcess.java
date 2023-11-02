686f35c54f2ea7bcad54017a94de699b62f8a152
SaminZou <822085977@qq.com>
2023-11-02 12:03:20 +0800

    ✨ Feat: enhance code tips

--- codetips/src/template/BatchProcess.java
+++ codetips/src/template/BatchProcess.java
@@ -8,40 +8,46 @@ package template;
  */
 public class BatchProcess {
 
+    // 初始化数据
+    private static int OFFSET = 0;
+    private static int DONE_COUNT = 0;
+    // 批次处理频率
+    private static int BATCH_NUM = 50;
+
     public static void main(String[] args) {
-        new BatchProcess().batch();
+        new BatchProcess().batch(2751);
     }
 
-    public void batch() {
-        // 初始化数据
-        int offset = 0;
-        int doneNum = 0;
-
-        // 需要处理的数据总量
-        int count = 2751;
-        // 批次处理频率
-        int batchNum = 50;
-
+    /**
+     * 批处理方法
+     *
+     * @param count 需要处理的数据总量
+     */
+    public void batch(int count) {
         System.out.println("需要处理的数据总量：" + count);
         while (count - 50 > 0) {
-            // 逻辑处理
-            System.out.printf("sql：offset %s limit %s \n", offset, batchNum);
+            // TODO 业务代码
+
+            System.out.printf("sql：offset %s limit %s \n", OFFSET, BATCH_NUM);
 
             // 计数更新
-            offset += batchNum;
-            count -= batchNum;
-            doneNum += batchNum;
-            System.out.println("已处理：" + doneNum);
+            OFFSET += BATCH_NUM;
+            count -= BATCH_NUM;
+            DONE_COUNT += BATCH_NUM;
+            System.out.println("已处理：" + DONE_COUNT);
         }
 
+        // 处理最后一批次
         if (count > 0) {
-            // 逻辑处理
-            System.out.printf("sql：offset %s limit %s \n", offset, batchNum);
+            // TODO 业务代码
 
-            System.out.printf("开始 %s \n", offset);
+            System.out.printf("sql：offset %s limit %s \n", OFFSET, BATCH_NUM);
+
+            System.out.printf("开始 %s \n", OFFSET);
             // 计数更新
-            doneNum += count;
-            System.out.println("已处理：" + doneNum);
+            DONE_COUNT += count;
+
+            System.out.println("已处理：" + DONE_COUNT);
         }
     }
 }
