204d9d35b80adf9e9d85736fce8baaab185b4136
SaminZou <822085977@qq.com>
2023-11-10 16:00:15 +0800

    ✨ Feat: enhance design pattern

--- designpattern/src/behavioural/strategy/Client.java
+++ designpattern/src/behavioural/strategy/Client.java
@@ -1,11 +1,12 @@
 package behavioural.strategy;
 
+import behavioural.strategy.strategy.ConcreteStrategyA;
 import behavioural.strategy.strategy.ConcreteStrategyB;
 
 /**
  * 策略模式
  *
- * <p>通过多态，让使用者感知不到内部策略的变化，策略方法本身是主体
+ * <p> 通过多态，让使用者感知不到内部策略的变化，策略方法本身是主体
  *
  * @author samin
  * @date 2021-01-05
@@ -13,8 +14,10 @@ import behavioural.strategy.strategy.ConcreteStrategyB;
 public class Client {
 
     public static void main(String[] args) {
-        // 操控比赛，这场要输
-        Context context = new Context(new ConcreteStrategyB());
+        Context context = new Context(new ConcreteStrategyA());
+        context.contextInterface();
+
+        context = new Context(new ConcreteStrategyB());
         context.contextInterface();
     }
 }
--- designpattern/src/behavioural/strategy/strategy/ConcreteStrategyA.java
+++ designpattern/src/behavioural/strategy/strategy/ConcreteStrategyA.java
@@ -4,7 +4,6 @@ public class ConcreteStrategyA implements Strategy {
 
     @Override
     public void algorithmLogic() {
-        // 具体的算法逻辑（输了比赛）
         System.out.println("第一场：上等马vs上等马  第二场：中等马vs中等马  第三场：下等马vs下等马  赛果：输！");
     }
 }
