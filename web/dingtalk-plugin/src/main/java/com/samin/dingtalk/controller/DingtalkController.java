7648b45dd568666d826ee996372e6023a461da98
SaminZou <822085977@qq.com>
2023-11-28 16:19:54 +0800

    ✨ Feat: enhance web coding

--- web/dingtalk-plugin/src/main/java/com/samin/dingtalk/controller/DingtalkController.java
+++ web/dingtalk-plugin/src/main/java/com/samin/dingtalk/controller/DingtalkController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.samin.dingtalk.pojo.req.AlertReq;
 import com.samin.dingtalk.service.DingtalkService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Api("钉钉通知控制类")
 public class DingtalkController {
 
     private final ObjectMapper objectMapper;
     private final DingtalkService dingtalkService;
 
+    @ApiOperation("钉钉通知接口")
     @PostMapping("/dingtalk")
     public String dingtalk(@RequestBody AlertReq req) throws JsonProcessingException {
         log.info("请求内容为：{}", objectMapper.writeValueAsString(req));
--- web/dingtalk-plugin/src/main/java/com/samin/dingtalk/pojo/req/AlertReq.java
+++ web/dingtalk-plugin/src/main/java/com/samin/dingtalk/pojo/req/AlertReq.java
@@ -1,18 +1,26 @@
 package com.samin.dingtalk.pojo.req;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import lombok.Data;
 
 @Data
+@ApiModel("告警通知")
 public class AlertReq {
 
+    @ApiModelProperty("接受者")
     private String receiver;
 
+    @ApiModelProperty("状态")
     private String status;
 
+    @ApiModelProperty("告警信息")
     private List<Alerts> alerts;
 
+    @ApiModelProperty("通用标签")
     private CommonLabels commonLabels;
 
+    @ApiModelProperty("通用注解")
     private CommonAnnotations commonAnnotations;
 }
