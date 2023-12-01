e644f108fa69f9f0de5c8c7354118672de84c7a2
SaminZou <822085977@qq.com>
2023-12-01 14:15:38 +0800

    ✨ Feat: enhance web coding

---
+++ codetips/src/collection/q3/ArrayStreamUseCase.java
@@ -0,0 +1,14 @@
+package collection.q3;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ArrayStreamUseCase {
+
+    public static void main(String[] args) {
+        String[] strs = new String[]{"a", "b"};
+        strs = Stream.concat(Arrays.stream(strs), Stream.of("c"))
+                .toArray(String[]::new);
+        System.out.println(Arrays.toString(strs));
+    }
+}
