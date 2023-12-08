19a7a5c253f04de36b62f69896416828eeadadfb
samin <822085977@qq.com>
2023-12-08 21:09:52 +0800

    ✨ Feat: enhance shell tips

--- shelltips/week-version.sh
+++ shelltips/week-version.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-branchname=bibt.$(date +%Y%m.%d)
-old_version=bibt.$(date -d "7 days ago" +%Y%m.%d)
+branchname=samin.$(date +%Y%m.%d)
+old_version=samin.$(date -d "7 days ago" +%Y%m.%d)
 
 # 测试指定时间
-#branchname=bibt.$(date -d "2023-07-31" +%Y%m.%d)
-#old_version=bibt.$(date -d "2023-07-31 - 7 days" +%Y%m.%d)
+#branchname=samin.$(date -d "2023-07-31" +%Y%m.%d)
+#old_version=samin.$(date -d "2023-07-31 - 7 days" +%Y%m.%d)
 
-if test "$old_version" = "bibt.202307.24"; then
-    old_version=bibt.2307.4
+if test "$old_version" = "samin.202307.24"; then
+    old_version=samin.2307.4
 fi
 
 echo "本周版本为：$branchname"
