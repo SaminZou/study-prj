903bad1c9eba0d515b25f42d04e1c59a9cbab665
SaminZou <822085977@qq.com>
2023-12-14 17:39:10 +0800

    ✨ Feat: enhance code tips

--- codetips/src/collection/q1/SortObj.java
+++ codetips/src/collection/q1/SortObj.java
@@ -18,7 +18,6 @@ public class SortObj implements Comparable<SortObj> {
 
     @Override
     public int compareTo(SortObj o) {
-        // System.out.println("使用 Comparable 接口实现方法排序");
         // 实现升序
         return this.weight - o.weight;
     }
