b81f94bd9994e30c68acc2962a30655d362efd04
SaminZou <822085977@qq.com>
2023-12-15 15:15:31 +0800

    ✨ Feat: enhance web coding

--- web/demo-module/redis-demo/src/main/java/com/samin/redis/RedisDemoApplication.java
+++ web/demo-module/redis-demo/src/main/java/com/samin/redis/RedisDemoApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * @EnableCaching 开启注解缓存功能
+ * <p>
+ * Description: @EnableCaching 开启注解缓存功能
+ * <p>
+ * Created By: Samin
+ * <p>
+ * Created Date: 2023-12-15
+ */
 @EnableCaching
 @SpringBootApplication
 public class RedisDemoApplication {
