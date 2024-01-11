9192ae24f3ed5975288c8730cbd97a83a174893d
SaminZou <822085977@qq.com>
2024-01-11 12:22:41 +0800

    ✨ Feat: enhance web coding

--- web/demo-module/use-case-demo/src/main/java/com/samin/usecase/valid/ValidQueryParamController.java
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/valid/ValidQueryParamController.java
@@ -1,18 +1,32 @@
 package com.samin.usecase.valid;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Validated
 @RestController
 public class ValidQueryParamController {
 
     @GetMapping("/validQueryParam")
-    public String valid(@NotNull(message = "col 不能为空") String col,
-                        @NotNull(message = "col2 不能为空") @Min(value = 1, message = "最小值为 1") String col2) {
+    public String valid(@NotNull(message = "col 不能为空") String col, @NotNull(message = "col2 不能为空") @Min(value = 1, message = "最小值为 1") String col2) {
         return "success: " + col;
+    }
+
+    /**
+     * 前端入参为：?params=foo,bar
+     * 或：?params=foo&params=bar
+     *
+     * @param params
+     * @return
+     */
+    @GetMapping("/arraysParam")
+    public String valid(@RequestParam("params") List<String> params) {
+        return "success: " + params;
     }
 }
