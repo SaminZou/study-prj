8bb8d8144e23ed93a366d2cd744c1a34a056927b
SaminZou <822085977@qq.com>
2024-02-26 18:55:26 +0800

    🚀 Deploy: add docker shell

---
+++ docker/sh/influxdb1.8.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run --name=influxdb-dev -itd --restart always \
+-p 8083:8083 \
+-p 8086:8086 \
+-e INFLUXDB_ADMIN_USER=influxdb2024 \
+-e INFLUXDB_ADMIN_PASSWORD=influxdb2024 \
+-v /mnt/data/influxdb:/var/lib/influxdb \
+influxdb:1.8.3
