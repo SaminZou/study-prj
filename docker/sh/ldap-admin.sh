f5bf40fce5a4b9f19ca04050f4e5c9d841a50752
SaminZou <822085977@qq.com>
2024-02-29 14:37:27 +0800

    🚀 Deploy: add docker shell

---
+++ docker/sh/ldap-admin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run --name ldap-admin \
+-p 6443:80 -itd \
+-e LDAP_SERVER_NAME="ldap" \
+-e LDAP_SERVER_BASE_DN="cn=admin,dc=example,dc=org" \
+-e LDAP_SERVER_HOST="samin.local" \
+-e LDAP_SERVER_PORT="389" \
+accenture/adop-ldap-phpadmin
