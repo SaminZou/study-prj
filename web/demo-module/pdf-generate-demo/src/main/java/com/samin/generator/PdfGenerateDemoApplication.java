5cbd11a21ca904a598dd3a333e687659cd9ebe93
SaminZou <822085977@qq.com>
2024-03-05 10:38:11 +0800

    ✨ Feat: add web coding

---
+++ web/demo-module/pdf-generate-demo/src/main/java/com/samin/generator/PdfGenerateDemoApplication.java
@@ -0,0 +1,12 @@
+package com.samin.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfGenerateDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfGenerateDemoApplication.class, args);
+    }
+}
