d33b88929dd88827d793b5ff358f89bfc32a00a3
SaminZou <822085977@qq.com>
2024-03-08 15:12:29 +0800

    ✨ Feat: enhance web coding

--- web/demo-module/pdf-generate-demo/src/main/java/com/samin/generator/service/EmailService.java
+++ web/demo-module/pdf-generate-demo/src/main/java/com/samin/generator/service/EmailService.java
@@ -3,11 +3,12 @@ package com.samin.generator.service;
 import com.samin.generator.entity.MailProperties;
 import com.samin.generator.repository.MailPropertiesRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 @Service
@@ -21,11 +22,21 @@ public class EmailService {
         JavaMailSenderImpl sender = getMailSender(mailProperties);
 
         // 简单文本
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(email);
-        mailMessage.setFrom(mailProperties.getUserName());
-        mailMessage.setSubject("这是测试邮件");
-        mailMessage.setText(content);
+        // SimpleMailMessage mailMessage = new SimpleMailMessage();
+        // mailMessage.setTo(email);
+        // mailMessage.setFrom(mailProperties.getUserName());
+        // mailMessage.setSubject("这是测试邮件");
+        // mailMessage.setText(content);
+
+        // HTML
+        MimeMessage mailMessage = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mailMessage, true);
+        helper.setTo(email);
+        helper.setFrom(mailProperties.getUserName());
+        // 抄送
+        // helper.setCc(email);
+        helper.setSubject("这是测试邮件");
+        helper.setText(content, true);
 
         sender.send(mailMessage);
     }
