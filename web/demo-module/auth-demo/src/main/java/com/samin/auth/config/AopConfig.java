1a610a4ee3e9ebc2afade19702232de63fef61e9
SaminZou <822085977@qq.com>
2024-03-19 18:13:45 +0800

    ✨ Feat: enhance web coding

---
+++ web/demo-module/auth-demo/src/main/java/com/samin/auth/config/AopConfig.java
@@ -0,0 +1,105 @@
+package com.samin.auth.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+/**
+ * API 接口认证
+ * <p>
+ * Description: 使用 appkey 和 appsecret 进行 API 接口认证
+ * <p>
+ * Created By: Samin
+ * <p>
+ * Created Date: 2024-03-18
+ */
+@Slf4j
+@Aspect
+@Component
+public class AopConfig {
+
+    /**
+     * 定义一个切入点
+     */
+    @Pointcut("execution(* com.samin.auth.controller.ApiController.*(..))")
+    public void pc1() {
+
+    }
+
+    /**
+     * 前置通知
+     *
+     * @param jp
+     */
+    @Before(value = "pc1()")
+    public void before(JoinPoint jp) {
+        String name = jp.getSignature()
+                .getName();
+        Object[] args = jp.getArgs();
+        log.info(name + "方法开始执行...");
+        log.info("方法参数: " + args[0]);
+    }
+
+    /**
+     * 后置通知
+     *
+     * @param jp
+     */
+    @After(value = "pc1()")
+    public void after(JoinPoint jp) {
+        String name = jp.getSignature()
+                .getName();
+        log.info(name + "方法执行结束...");
+    }
+
+    /**
+     * 返回通知
+     *
+     * @param jp
+     * @param result
+     */
+    @AfterReturning(value = "pc1()", returning = "result")
+    public void afterReturning(JoinPoint jp, Object result) {
+        String name = jp.getSignature()
+                .getName();
+        log.info(name + "方法返回值为：" + result);
+    }
+
+    /**
+     * 异常通知
+     *
+     * @param jp
+     * @param e
+     */
+    @AfterThrowing(value = "pc1()", throwing = "e")
+    public void afterThrowing(JoinPoint jp, Exception e) {
+        String name = jp.getSignature()
+                .getName();
+        log.info(name + "方法抛异常了，异常是：" + e.getMessage());
+    }
+
+    /**
+     * 环绕通知
+     *
+     * @param pjp
+     * @return
+     * @throws Throwable
+     */
+    @Around("pc1()")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        String name = pjp.getSignature()
+                .getName();
+        // 统计方法执行时间
+        long start = Instant.now()
+                .toEpochMilli();
+        Object result = pjp.proceed();
+        long end = Instant.now()
+                .toEpochMilli();
+        log.info(name + "方法执行时间为：" + (end - start) + " ms");
+        return result;
+    }
+}
