fa23c2f5c4f525b929459e743d9c60dbfa8de146
SaminZou <822085977@qq.com>
2024-03-22 12:13:20 +0800

    ✨ Feat: add job-center project

---
+++ job-center/src/main/java/com/samin/jobcenter/JobCenterApplication.java
@@ -0,0 +1,13 @@
+package com.samin.jobcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobCenterApplication.class, args);
+    }
+
+}
