1dcde6d1eb959a732a629482f9a64501ee4ea46e
SaminZou <822085977@qq.com>
2024-03-28 14:16:13 +0800

    ✨ Feat: add job worker

---
+++ job-worker/job-worker-a/src/main/java/com/samin/jobworkera/JobWorkerAApplication.java
@@ -0,0 +1,13 @@
+package com.samin.jobworkera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobWorkerAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobWorkerAApplication.class, args);
+    }
+
+}
---
+++ job-worker/job-worker-b/src/main/java/com/samin/jobworkerb/JobWorkerBApplication.java
@@ -0,0 +1,13 @@
+package com.samin.jobworkerb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobWorkerBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobWorkerBApplication.class, args);
+    }
+
+}
