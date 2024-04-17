ef34245d3aa3edf61cdf50d5e74313e5d57bb7ae
SaminZou <822085977@qq.com>
2024-04-17 14:29:47 +0800

    ✨ Feat: enhance job admin

--- job-center/job-admin/src/main/java/com/samin/jobadmin/controller/JobLogController.java
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/controller/JobLogController.java
@@ -3,9 +3,9 @@ package com.samin.jobadmin.controller;
 import com.samin.jobadmin.bean.BaseResp;
 import com.samin.jobadmin.bean.JobLogVo;
 import com.samin.jobadmin.bean.PageReq;
+import com.samin.jobadmin.bean.PageResp;
 import com.samin.jobadmin.service.JobLogService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +18,7 @@ public class JobLogController {
     private final JobLogService jobLogService;
 
     @GetMapping("/page")
-    public BaseResp<Page<JobLogVo>> page(PageReq<Void> req) {
+    public BaseResp<PageResp<JobLogVo>> page(PageReq<Void> req) {
         return BaseResp.success(jobLogService.page(req));
     }
 }
--- job-center/job-admin/src/main/java/com/samin/jobadmin/service/JobLogService.java
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/service/JobLogService.java
@@ -2,10 +2,10 @@ package com.samin.jobadmin.service;
 
 import com.samin.jobadmin.bean.JobLogVo;
 import com.samin.jobadmin.bean.PageReq;
+import com.samin.jobadmin.bean.PageResp;
 import com.samin.jobadmin.entity.JobLog;
 import com.samin.jobadmin.repository.JobLogRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -17,10 +17,11 @@ public class JobLogService {
 
     private final JobLogRepository jobLogRepository;
 
-    public Page<JobLogVo> page(PageReq<Void> req) {
+    public PageResp<JobLogVo> page(PageReq<Void> req) {
         Sort sort = Sort.by(Sort.Direction.DESC, "createTime");
         Pageable pageable = PageRequest.of(req.getPage(), req.getSize(), sort);
-        Page<JobLog> jobLogs = jobLogRepository.findAll(pageable);
+        PageResp<JobLog> jobLogs = PageResp.success(jobLogRepository.findAll(pageable));
+
         return jobLogs.map(JobLogVo::getInstance);
     }
 }
