db7b8d1337f627647d354b28758f828996018a5c
SaminZou <822085977@qq.com>
2024-04-24 11:19:29 +0800

    ✨ Feat: enhance job admin

---
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/controller/JobWorkerController.java
@@ -0,0 +1,13 @@
+package com.samin.jobadmin.controller;
+
+import com.samin.jobadmin.bean.*;
+import com.samin.jobadmin.service.JobService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/jobWorker")
+@RequiredArgsConstructor
+public class JobWorkerController {
+
+}
---
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/controller/JobWorkerGroupController.java
@@ -0,0 +1,12 @@
+package com.samin.jobadmin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobWorkerGroup")
+@RequiredArgsConstructor
+public class JobWorkerGroupController {
+
+}
