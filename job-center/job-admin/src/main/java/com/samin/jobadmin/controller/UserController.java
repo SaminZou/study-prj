112880b7877373d88151467928ed4bf13534bd40
SaminZou <822085977@qq.com>
2024-05-21 08:53:21 +0800

    ✨ Feat: enhance job admin

---
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/controller/UserController.java
@@ -0,0 +1,2 @@
+package com.samin.jobadmin.controller;public class UserController {
+}
---
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.samin.jobadmin.repository;
+
+import com.samin.jobadmin.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
