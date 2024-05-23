e80c6d3bfe6bb889c3b9329f1c051c06f015189b
SaminZou <822085977@qq.com>
2024-05-23 12:08:01 +0800

    ✨ Feat: enhance job admin

--- job-center/job-admin/src/main/java/com/samin/jobadmin/controller/UserController.java
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.samin.jobadmin.controller;
 
+import com.samin.jobadmin.bean.JobVo;
+import com.samin.jobsdk.bean.BaseResp;
+import com.samin.jobsdk.bean.PageReq;
+import com.samin.jobsdk.bean.PageResp;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +13,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
-
 
 }
---
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/service/UserService.java
@@ -0,0 +1,9 @@
+package com.samin.jobadmin.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
