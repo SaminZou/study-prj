8075de28c725a3644735c1377d42b8adc9da1000
SaminZou <822085977@qq.com>
2024-05-27 19:48:11 +0800

    ✨ Feat: enhance job admin

--- job-center/job-admin/src/main/java/com/samin/jobadmin/bean/UserVo.java
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/bean/UserVo.java
@@ -1,9 +1,16 @@
 package com.samin.jobadmin.bean;
 
+import com.samin.jobadmin.entity.User;
 import lombok.Data;
 
 @Data
 public class UserVo {
+
+    public static UserVo getInstance(User user) {
+      UserVo ins = new UserVo();
+
+      return ins;
+    }
 
     private Integer id;
 
--- job-center/job-admin/src/main/java/com/samin/jobadmin/service/UserService.java
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/service/UserService.java
@@ -1,16 +1,27 @@
 package com.samin.jobadmin.service;
 
 import com.samin.jobadmin.bean.UserVo;
+import com.samin.jobadmin.entity.User;
+import com.samin.jobadmin.repository.UserRepository;
 import com.samin.jobsdk.bean.PageReq;
 import com.samin.jobsdk.bean.PageResp;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
     public PageResp<UserVo> page(PageReq<Void> req) {
-        return null;
+        Sort sort = Sort.by(Sort.Direction.DESC, "createTime");
+        Pageable pageable = PageRequest.of(req.getPage(), req.getSize(), sort);
+        PageResp<User> users = PageResp.success(userRepository.findAll(pageable));
+
+        return users.map(UserVo::getInstance);
     }
 }
