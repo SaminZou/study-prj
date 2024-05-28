ca2497a3dfae37c31ba72e936a6d6961a624a5a6
SaminZou <822085977@qq.com>
2024-05-28 16:39:38 +0800

    ✨ Feat: enhance job admin

--- job-center/job-admin/src/main/java/com/samin/jobadmin/bean/UserVo.java
+++ job-center/job-admin/src/main/java/com/samin/jobadmin/bean/UserVo.java
@@ -3,13 +3,25 @@ package com.samin.jobadmin.bean;
 import com.samin.jobadmin.entity.User;
 import lombok.Data;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
 @Data
 public class UserVo {
 
     public static UserVo getInstance(User user) {
-      UserVo ins = new UserVo();
+        UserVo ins = new UserVo();
 
-      return ins;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        ins.setId(user.getId());
+        ins.setLoginName(user.getLoginName());
+        ins.setPassword(user.getPassword());
+        ins.setIsEnable(user.getIsEnable());
+        ins.setCreateTime(Objects.nonNull(user.getCreateTime()) ? dtf.format(user.getCreateTime()) : "");
+        ins.setUpdateTime(Objects.nonNull(user.getUpdateTime()) ? dtf.format(user.getUpdateTime()) : "");
+
+        return ins;
     }
 
     private Integer id;
