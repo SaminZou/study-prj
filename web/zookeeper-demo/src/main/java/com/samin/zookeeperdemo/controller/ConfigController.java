c26e73adb5578ab633469d4d9a319ae92eb197a0
SaminZou <822085977@qq.com>
2024-07-13 10:22:19 +0800

    ✨ Feat: enhance zookeeper demo

--- web/zookeeper-demo/src/main/java/com/samin/zookeeperdemo/controller/ConfigController.java
+++ web/zookeeper-demo/src/main/java/com/samin/zookeeperdemo/controller/ConfigController.java
@@ -4,10 +4,9 @@ import com.samin.zookeeperdemo.bean.ConfigVO;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.zookeeper.CreateMode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/config")
@@ -29,6 +28,26 @@ public class ConfigController {
         curatorFramework.setData()
                 .forPath("/samin-config", configVO.getValue()
                         .getBytes());
+
+        return "success";
+    }
+
+    @GetMapping("/getGlobalId")
+    public String getGlobalId() throws Exception {
+        String path = "/samin-global/id-";
+        String destPath = curatorFramework.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
+                .forPath(path);
+
+        if (Objects.nonNull(destPath)) {
+            // 获取末尾的序号
+            int index = destPath.lastIndexOf(path);
+            if (index >= 0) {
+                index += path.length();
+                return index <= destPath.length() ? destPath.substring(index) : "";
+            }
+        }
 
         return "success";
     }
