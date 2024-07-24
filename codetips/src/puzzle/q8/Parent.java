b1567422ae1a7b80d05ac3c5cfd724209ca88dec
SaminZou <822085977@qq.com>
2024-07-24 18:27:06 +0800

    ✨ Feat: enhance code tips

---
+++ codetips/src/puzzle/q8/Parent.java
@@ -0,0 +1,12 @@
+package puzzle.q8;
+
+public class Parent {
+
+    public void testPublic(){
+        System.out.println("parent.test.public");
+    }
+
+    private void test() {
+        System.out.println("parent.test");
+    }
+}
