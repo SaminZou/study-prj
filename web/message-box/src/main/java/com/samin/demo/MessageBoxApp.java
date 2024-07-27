b61cc9b8738c75ce41cfa605e847430623937b9f
SaminZou <822085977@qq.com>
2024-07-27 11:42:15 +0800

    ✨ Feat: add message box

---
+++ web/message-box/src/main/java/com/samin/demo/MessageBoxApp.java
@@ -0,0 +1,12 @@
+package com.samin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBoxApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageBoxApp.class, args);
+    }
+}
