f81205bd213d95ab17dba1aea27a5817960f9d59
samin <822085977@qq.com>
2024-08-04 18:53:30 +0800

    ✨ Feat: add spring cloud alibaba demo

---
+++ web/spring-cloud-alibaba-study/spring-cloud-alibaba-demo2/src/main/java/com/samin/demo/SpringCloudAlibabaApp2.java
@@ -0,0 +1,12 @@
+package com.samin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaApp2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaApp2.class, args);
+    }
+}
