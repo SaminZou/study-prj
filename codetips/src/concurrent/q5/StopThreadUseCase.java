ee70c24ef0840a37f57717f6b44aca864302c93a
samin <822085977@qq.com>
2024-08-10 22:06:36 +0800

    ✨ Feat: enhance code tips

--- codetips/src/concurrent/q5/StopThreadUseCase.java
+++ codetips/src/concurrent/q5/StopThreadUseCase.java
@@ -40,9 +40,7 @@ public class StopThreadUseCase {
     static void stopThreadV2() {
         TaskThread taskThread = new TaskThread();
 
-        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(9, 20, 10, TimeUnit.MINUTES, new LinkedBlockingQueue<>(100),
-                                                                 r -> new Thread(r, "show-the-error-thread-"
-                                                                         + new Random().nextInt(999)));
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(9, 20, 10, TimeUnit.MINUTES, new LinkedBlockingQueue<>(100), r -> new Thread(r, "show-the-error-thread-" + new Random().nextInt(999)));
         Future<?> future = poolExecutor.submit(taskThread);
 
         try {
@@ -61,5 +59,42 @@ public class StopThreadUseCase {
         Thread thread = new Thread(sleepThread, "sleepThread");
         thread.start();
         thread.interrupt();
+    }
+
+    static class SleepThread implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("线程开始。。。");
+                Thread.sleep(200000);
+                System.out.println("线程结束。");
+            } catch (InterruptedException e) {
+                System.out.println("在沉睡中被停止, 进入catch， 调用isInterrupted()方法的结果是：" + Thread.currentThread()
+                        .isInterrupted());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class TaskThread implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 500000; i++) {
+                    if (Thread.interrupted()) {
+                        System.out.println("线程已经终止， for循环不再执行");
+                        throw new InterruptedException();
+                    }
+                    System.out.println("i=" + (i + 1));
+                }
+
+                System.out.println("这是for循环外面的语句，如果不是抛异常而是 break，也会被执行");
+            } catch (InterruptedException e) {
+                System.out.println("进入MyThread.java类中的catch了。。。");
+                e.printStackTrace();
+            }
+        }
     }
 }
