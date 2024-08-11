70b744b2e0b399bcfc8e5f170cd00314d96f1827
samin <822085977@qq.com>
2024-08-11 23:43:13 +0800

    ✨ Feat: enhance spring cloud alibaba study

--- web/spring-cloud-alibaba-study/spring-cloud-alibaba-demo2/spring-cloud-alibaba-demo-app2/src/main/java/com/samin/demo/SpringCloudAlibabaApp2.java
+++ web/spring-cloud-alibaba-study/spring-cloud-alibaba-demo2/spring-cloud-alibaba-demo-app2/src/main/java/com/samin/demo/SpringCloudAlibabaApp2.java
@@ -2,11 +2,13 @@ package com.samin.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class SpringCloudAlibabaApp3 {
+@EnableDiscoveryClient
+public class SpringCloudAlibabaApp2 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudAlibabaApp3.class, args);
+        SpringApplication.run(SpringCloudAlibabaApp2.class, args);
     }
 }
