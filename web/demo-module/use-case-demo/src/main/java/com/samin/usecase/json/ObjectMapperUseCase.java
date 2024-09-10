5b6f21c6c4a6618abbd64a3c28026308d20a7af7
SaminZou <822085977@qq.com>
2024-09-10 08:46:34 +0800

    ✨ Feat: enhance use case

---
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/json/ObjectMapperUseCase.java
@@ -0,0 +1,34 @@
+package com.samin.usecase.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/json")
+public class ObjectMapperUseCase {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping("/generics")
+    public BaseResp<JsonData> generics() throws JsonProcessingException {
+        String json = "{\"code\":0, \"msg\":\"success\",\"data\":{\"name\":\"test\"}}";
+        // 这种写法会报 Unchecked assignment
+        // BaseResp<JsonData> resp = objectMapper.readValue(json, BaseResp.class);
+        BaseResp<JsonData> resp = objectMapper.readValue(json, new TypeReference<BaseResp<JsonData>>() {
+        });
+        return resp;
+    }
+
+    @Data
+    public static class JsonData {
+
+        private String name;
+    }
+}
