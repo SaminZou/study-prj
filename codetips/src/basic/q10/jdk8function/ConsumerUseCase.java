f19648afc3d5bd89c44d2ae4c44e59fbc164be91
SaminZou <822085977@qq.com>
2024-09-13 08:01:53 +0800

    ✨ Feat: enhance code tips

--- codetips/src/basic/q10/jdk8function/ConsumerUseCase.java
+++ codetips/src/basic/q10/jdk8function/ConsumerUseCase.java
@@ -19,5 +19,34 @@ public class ConsumerUseCase {
         // Stream 的 forEach / peek 方法就是传入一个实现 Consumer 的实例化
         Stream<String> someStr = Stream.of("a", "B", "c");
         someStr.forEach(consumerImpl);
+
+        test(true, s -> s.setName("test"));
+        test(false, null);
+    }
+
+    public static void test(boolean isShow, Consumer<Obj> consumer) {
+        Obj obj = new Obj();
+        if (isShow) {
+            consumer.accept(obj);
+            System.out.println(obj);
+        } else {
+            System.out.println(obj);
+        }
+    }
+
+    public static class Obj {
+
+        private String name = "none";
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Obj{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
     }
 }
