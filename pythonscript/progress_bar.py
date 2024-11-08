e29f9c32f201d60f8a5bcfc3614ccf0831c2090e
SaminZou <822085977@qq.com>
2024-11-08 19:29:43 +0800

    ✨ Feat: add python script

---
+++ pythonscript/progress_bar.py
@@ -0,0 +1,28 @@
+import time
+
+def progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=50, fill='█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                     (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end='\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+if __name__ == "__main__":
+    for i in range(1, 101):
+        time.sleep(0.1)
+        progress_bar(i + 1, 100, prefix='Progress:',
+                     suffix='Complete', length=50)
