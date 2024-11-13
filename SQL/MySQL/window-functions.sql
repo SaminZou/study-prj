eccedee4eedc9bc8dff3563c09e7bf2780ff6758
SaminZou <822085977@qq.com>
2024-11-13 10:51:37 +0800

    ✨ Feat: enhance SQL

--- SQL/MySQL/window-functions.sql
+++ SQL/MySQL/window-functions.sql
@@ -1,1 +1,10 @@
-// TODO 窗口函数
+-- 窗口函数
+
+-- 根据 create_date 聚合 device_nums，统计每一天的设备总数
+SELECT DISTINCT create_date,
+                SUM(device_nums) OVER (
+           PARTITION BY create_date
+       ) AS device_nums
+FROM project_device
+ORDER BY create_date DESC
+;
