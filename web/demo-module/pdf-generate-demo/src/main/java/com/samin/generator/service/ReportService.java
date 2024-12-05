92bc0380ba8cdab590b358179daffb7ca744a5c0
SaminZou <822085977@qq.com>
2024-12-05 08:54:50 +0800

    ✨ Feat: enhance pdf generate demo

--- web/demo-module/pdf-generate-demo/src/main/java/com/samin/generator/service/ReportService.java
+++ web/demo-module/pdf-generate-demo/src/main/java/com/samin/generator/service/ReportService.java
@@ -1,6 +1,9 @@
 package com.samin.generator.service;
 
 import com.lowagie.text.pdf.BaseFont;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
@@ -8,35 +11,33 @@ import org.thymeleaf.context.Context;
 import org.xhtmlrenderer.pdf.ITextFontResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 @Service
 @RequiredArgsConstructor
 public class ReportService {
 
-    private final TemplateEngine templateEngine;
+  private final TemplateEngine templateEngine;
 
-    public String report() throws IOException {
-        // 创建Thymeleaf上下文
-        Context context = new Context();
-        // 设置要传递给Thymeleaf模板的数据（例如，从后端获取的报表数据）
-        context.setVariable("name", "中文标题");
-        context.setVariable("content", "Content with 中文。");
-        // 渲染 Thymeleaf 模板
-        String htmlContent = templateEngine.process("report2PDF", context);
+  public String report() throws IOException {
+    //  修改路径
+    String path = "C:\\Users\\samin\\Desktop\\report.pdf";
 
-        ITextRenderer renderer = new ITextRenderer();
+    // 创建Thymeleaf上下文
+    Context context = new Context();
+    // 设置要传递给Thymeleaf模板的数据（例如，从后端获取的报表数据）
+    context.setVariable("name", "中文标题");
+    context.setVariable("content", "Content with 中文。");
 
-        // 配置中文字体
-        ITextFontResolver fontResolver = renderer.getFontResolver();
-        fontResolver.addFont("static/SimSun.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+    // 渲染 Thymeleaf 模板
+    String htmlContent = templateEngine.process("report2PDF", context);
 
-        renderer.setDocumentFromString(htmlContent);
-        renderer.layout();
-        renderer.createPDF(Files.newOutputStream(Paths.get("C:\\Users\\samin\\Desktop\\report.pdf")));
+    ITextRenderer renderer = new ITextRenderer();
+    // 配置中文字体
+    ITextFontResolver fontResolver = renderer.getFontResolver();
+    fontResolver.addFont("static/SimSun.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+    renderer.setDocumentFromString(htmlContent);
+    renderer.layout();
+    renderer.createPDF(Files.newOutputStream(Paths.get(path)));
 
-        return htmlContent;
-    }
+    return htmlContent;
+  }
 }
