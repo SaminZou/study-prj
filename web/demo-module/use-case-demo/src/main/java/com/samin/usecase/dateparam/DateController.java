182d8ed188ae6318ab965b8ba28f327b398c4aa3
SaminZou <822085977@qq.com>
2024-12-26 09:14:08 +0800

    ✨ Feat: enhance web coding

---
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/dateparam/DateController.java
@@ -0,0 +1,18 @@
+package com.samin.usecase.dateparam;
+
+import java.time.format.DateTimeFormatter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class DateController {
+
+  @PostMapping("/date/req")
+  public void dateReq(@RequestBody DateReq req) {
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    log.info("时间段： {} ~ {}", req.getStartTime().format(dtf), req.getEndTime().format(dtf));
+  }
+}
---
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/dateparam/DateReq.java
@@ -0,0 +1,19 @@
+package com.samin.usecase.dateparam;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+public class DateReq {
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  private LocalDateTime startTime;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  private LocalDateTime endTime;
+}
