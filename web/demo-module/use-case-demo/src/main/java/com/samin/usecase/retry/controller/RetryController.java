143206144644ae1d83c72341a0a2a2c03a55b3ab
SaminZou <822085977@qq.com>
2025-01-02 10:17:34 +0800

    ✨ Feat: enhance web coding

--- web/demo-module/use-case-demo/src/main/java/com/samin/usecase/retry/controller/RetryController.java
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/retry/controller/RetryController.java
@@ -4,18 +4,21 @@ import com.samin.usecase.retry.service.RetryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class RetryController {
 
-    private final RetryService retryService;
+  private final RetryService retryService;
 
-    @GetMapping("/retry/{code}")
-    public String retry(@PathVariable(value = "code") int code) throws Exception {
-        int test = retryService.retry(code);
-        System.out.println("返回结果：" + test);
-        return "success";
-    }
+  @GetMapping("/retry/{code}")
+  public String retry(
+      @PathVariable(value = "code") int code, @RequestParam(value = "isRetry") int isRetry)
+      throws Exception {
+    int test = retryService.retry(code, isRetry);
+    System.out.println("返回结果：" + test);
+    return "success";
+  }
 }
--- web/demo-module/use-case-demo/src/main/java/com/samin/usecase/retry/service/RetryService.java
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/retry/service/RetryService.java
@@ -1,5 +1,6 @@
 package com.samin.usecase.retry.service;
 
+import java.time.LocalTime;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.EnableRetry;
@@ -7,48 +8,48 @@ import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalTime;
-
 /**
  * 重试服务类
- * <p>
- * Description: 重试服务类，需要 @EnableRetry
- * <p>
- * Created By: Samin
- * <p>
- * Created Date: 2024-02-01
+ *
+ * <p>Description: 重试服务类，需要 @EnableRetry
+ *
+ * <p>Created By: Samin
+ *
+ * <p>Created Date: 2024-02-01
  */
 @Slf4j
 @Service
 @EnableRetry
 public class RetryService {
 
-    private int index = 0;
+  private int index = 0;
 
-    /**
-     * 注意 maxAttempts 的次数是包含了所有的执行次数
-     *
-     * @param code
-     * @return
-     * @throws Exception
-     */
-    @Retryable(value = Exception.class, maxAttempts = 2, backoff = @Backoff(delay = 2000, multiplier = 1.5))
-    public int retry(int code) throws Exception {
-        // param > 1 就可以观测 recover() 方法被执行
-        int param = 1;
-        log.info("业务方法被调用，时间：" + LocalTime.now());
-        if (index < param) {
-            index += 1;
-            throw new Exception("业务方法被调用，执行报错！");
-        }
-        log.info("业务方法调用成功");
-        return code;
+  /**
+   * 注意 maxAttempts 的次数是包含了所有的执行次数
+   *
+   * @param code
+   * @return
+   * @throws Exception
+   */
+  @Retryable(
+      value = Exception.class,
+      maxAttempts = 2,
+      backoff = @Backoff(delay = 2000, multiplier = 1.5))
+  public int retry(int code, int isRetry) throws Exception {
+    isRetry = isRetry > 0 ? 3 : isRetry;
+    log.info("业务方法被调用，时间：" + LocalTime.now());
+    if (index < isRetry) {
+      index += 1;
+      throw new Exception("业务方法被调用，执行报错！");
     }
+    log.info("业务方法调用成功");
+    return code;
+  }
 
-    @Recover
-    public int recover(Exception e, int code) {
-        log.info("业务方法调用失败，触发执行对应的方法回调！！！！");
-        //记日志到数据库，或者进行补救措施
-        return -1;
-    }
+  @Recover
+  public int recover(Exception e, int code) {
+    log.info("业务方法调用失败，触发执行对应的方法回调！！！！");
+    // 记日志到数据库，或者进行补救措施
+    return -1;
+  }
 }
