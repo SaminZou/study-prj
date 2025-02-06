3b7b7f99b6a4d85e0b049d103a19a4dade58cb8e
SaminZou <822085977@qq.com>
2025-02-06 15:21:34 +0800

    ✨ Feat: enhance web coding

---
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/apilogs/config/RequestTimingEventListener.java
@@ -0,0 +1,35 @@
+package com.samin.usecase.apilogs.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.ServletRequestHandledEvent;
+
+@Component
+@Slf4j
+public class RequestTimingEventListener implements ApplicationListener<ServletRequestHandledEvent> {
+
+    @Override
+    public void onApplicationEvent(ServletRequestHandledEvent event) {
+        // 获取请求失败的原因（ event.getFailureCause() ）
+        Throwable failureCause = event.getFailureCause();
+        String failureMessage = (failureCause == null) ? "" : failureCause.getMessage();
+
+        // 获取请求相关的信息
+        String clientAddress = event.getClientAddress();
+        String requestUrl = event.getRequestUrl();
+        String method = event.getMethod();
+
+        // event.getProcessingTimeMillis() 获取处理耗时
+        long processingTimeMillis = event.getProcessingTimeMillis();
+
+        // 日志输出请求处理信息
+        if (failureCause == null) {
+            log.info("客户端地址: {}，请求路径: {}，请求方法: {}，处理耗时: {} 毫秒", clientAddress, requestUrl, method,
+                     processingTimeMillis);
+        } else {
+            log.error("客户端地址: {}，请求路径: {}，请求方法: {}，处理耗时: {} 毫秒，错误信息: {}", clientAddress, requestUrl,
+                      method, processingTimeMillis, failureMessage);
+        }
+    }
+}
---
+++ web/demo-module/use-case-demo/src/main/java/com/samin/usecase/apilogs/doc.md
@@ -0,0 +1,21 @@
+# 实现功能
+
+- 请求来源的 IP 地址（客户端地址）
+- 请求的目标路径（请求 URL）
+- HTTP 方法
+- 处理耗时
+- 异常
+
+# ServletRequestHandledEvent 原理
+
+- Spring 的事件驱动机制
+
+- 观察者模式
+
+当请求被 DispatcherServlet 完整处理后，Spring 会通过 ApplicationEventPublisher 发布 ServletRequestHandledEvent。随后，所有实现了 ApplicationListener<ServletRequestHandledEvent> 的监听器会接收到这个事件并执行相应的处理逻辑。
+
+# 优点
+
+- 非侵入性
+- 不影响业务代码（性能监控的逻辑完全独立于业务实现，避免了代码耦合。）
+- 灵活扩展（开发者可以根据需求自定义监听器，轻松扩展功能。）
