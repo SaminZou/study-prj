82c65b5ff24a6283ba01f571d35917676068d1d7
SaminZou <822085977@qq.com>
2025-03-21 15:40:32 +0800

    ✨ Feat: enhance code tips

--- codetips/src/basic/ObjectsUseCase.java
+++ codetips/src/basic/ObjectsUseCase.java
@@ -3,7 +3,7 @@ package basic;
 import java.util.Objects;
 
 /**
- * Object 类用例
+ * Objects 类用例
  *
  * @author samin
  * @date 2020-12-31
@@ -11,24 +11,25 @@ import java.util.Objects;
 public class ObjectsUseCase {
 
     public static void main(String[] args) {
+        final String b = "123";
+
         String a = null;
-        String b = "123";
         // 这个会报空指针
         // System.out.println(a.equals(b));
 
-        // 正常需要这样来解决
+        // 正常会这样来解决
         if (a != null) {
             System.out.println(a.equals(b));
         }
 
         // 或者使用常量或者确定值来解决报空指针的问题
-        if ("123".equals(a)) {
+        if (b.equals(a)) {
         }
 
         /**
-         * JDK1.7之后提供 Objects 工具包解决这个问题
+         * JDK1.7 之后提供 Objects 工具包解决这个问题
          *
-         * <p>Objects 的 equals() 方法是先判断入参是否为空，然后调用 Object 的 equals() 方法进行比较
+         * <p>Objects 的 equals() 方法是先判断入参是否为空，然后调用 Objects 的 equals() 方法进行比较
          *
          * <p>原因在于 a.equals(b) 这种写法，当 a 为 null 值时，会报错
          */
