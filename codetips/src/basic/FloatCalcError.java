c0559a78199d024718d3e14d7cb3aef7bf056412
SaminZou <822085977@qq.com>
2025-04-15 09:53:29 +0800

    ✨ Feat: enhance code tips

--- codetips/src/basic/FloatCalcError.java
+++ codetips/src/basic/FloatCalcError.java
@@ -3,7 +3,7 @@ package basic;
 import java.math.BigDecimal;
 
 /**
- * 数字精度问题
+ * 浮点数运算精度问题
  *
  * <p>使用 java.math.BigDecimal 解决
  *
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 public class FloatCalcError {
 
     public static void main(String[] args) {
-        // 小数默认为 double 类型，所以声明 float 类型的变量需要后面加'f'或'F'，或者使用 (float)num 转换类型
+        // 小数默认为 double 类型，所以声明 float 类型的变量需要后面加 f 或 F ，或者使用 (float)num 转换类型
         float a = 68399.22F;
         float b = (float) 124052.96;
         // 出现精度问题，结果错误
