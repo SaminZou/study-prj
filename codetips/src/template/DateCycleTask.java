73dc613ad5ade73cb240b5cff484b91cab64f1f8
SaminZou <822085977@qq.com>
2025-04-18 09:19:53 +0800

    ✨ Feat: enhance code tips

--- codetips/src/template/DateCycleTask.java
+++ codetips/src/template/DateCycleTask.java
@@ -6,14 +6,14 @@ import java.time.format.DateTimeFormatter;
 public class DateCycleTask {
 
   public static void main(String[] args) {
-    // mock 2024-10-01 to 2024-10-11
+    // mock 2024-10-01 to 2024-10-11，左闭右开，所以入参是 2024-10-01 和 2024-10-12
     process(
         LocalDate.parse("2024-10-01", DateTimeFormatter.ofPattern("yyyy-MM-dd")),
         LocalDate.parse("2024-10-12", DateTimeFormatter.ofPattern("yyyy-MM-dd")));
   }
 
   /**
-   * 左闭右开区间
+   * 左闭右开区间（方法实现的约定俗成）
    *
    * @param start
    * @param end
