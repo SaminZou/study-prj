f7100949248b8a804fce1c4a34aec68efdc86e5f
SaminZou <822085977@qq.com>
2025-04-19 09:30:20 +0800

    ✨ Feat: enhance spring use case

--- web/spring-use-case/src/main/java/com/samin/usecase/value/ApplicationReaderController.java
+++ web/spring-use-case/src/main/java/com/samin/usecase/value/ApplicationReaderController.java
@@ -2,6 +2,7 @@ package com.samin.usecase.value;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,16 @@ public class ApplicationReaderController {
     @GetMapping("/orderTest")
     public String orderTest() {
         return orderTest;
+    }
+
+    /**
+     * 导致入参如果是 "orderTest" 会无法进入此方法
+     *
+     * @param test
+     * @return
+     */
+    @GetMapping("/{test}")
+    public String orderTest(@PathVariable String test) {
+        return test;
     }
 }
