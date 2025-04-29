3ac6a60bc7b455471369b5ce7beaae1538a7c916
SaminZou <822085977@qq.com>
2025-04-29 11:59:14 +0800

    ✨ Feat: add spring security oauth2 demo

---
+++ web/spring-security-oauth2-demo/authorization-server/src/main/java/com/samin/author/AuthorizationApplication.java
@@ -0,0 +1,12 @@
+package com.samin.author;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApplication.class, args);
+    }
+}
---
+++ web/spring-security-oauth2-demo/resource-server/src/main/java/com/samin/resource/ResourceApplication.java
@@ -0,0 +1,12 @@
+package com.samin.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApplication.class, args);
+    }
+}
