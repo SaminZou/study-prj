ad3f8cf947f3385dbd0c1b9cbe047603eb2b98ef
SaminZou <822085977@qq.com>
2025-05-08 21:38:49 +0800

    ✨ Feat: enhance spring use case

--- web/spring-use-case/src/main/java/com/samin/usecase/scheduler/config/TestScheduler.java
+++ web/spring-use-case/src/main/java/com/samin/usecase/scheduler/config/TestScheduler.java
@@ -1,7 +1,9 @@
 package com.samin.usecase.scheduler.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,10 +18,20 @@ public class TestScheduler {
      * 每隔 15 秒运行一次
      */
     @Scheduled(cron = "0/15 * * * * *")
-    public void test() {
+    public void test0() {
         log.info("每隔 15 秒运行一次: {}", LocalDateTime.now()
                                                         .format(DateTimeFormatter.ofPattern("HH:mm:ss")));
     }
+
+    /**
+     * 方法 2：每隔 15 秒运行一次
+     */
+    @Scheduled(fixedRate = 15 * 1000)
+    public void test1() {
+        log.info("每隔 15 秒运行一次: {}", LocalDateTime.now()
+                                                        .format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+    }
+
 
     /**
      * 每分钟的 05 秒运行一次
