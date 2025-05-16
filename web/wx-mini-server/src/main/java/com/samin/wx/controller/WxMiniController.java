36d0dc3d93a45e0d50e337f0378281aee2288b59
SaminZou <822085977@qq.com>
2025-05-16 11:27:46 +0800

    ✨ Feat: enhance wx mini server

--- web/wx-mini-server/src/main/java/com/samin/wx/controller/WxMiniController.java
+++ web/wx-mini-server/src/main/java/com/samin/wx/controller/WxMiniController.java
@@ -3,10 +3,14 @@ package com.samin.wx.controller;
 
 import cn.binarywang.wx.miniapp.api.WxMaService;
 import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
+import cn.binarywang.wx.miniapp.bean.WxMaPhoneNumberInfo;
+import com.samin.wx.model.GetPhoneNumberReq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +37,21 @@ public class WxMiniController {
                                                       .getSessionInfo(code);
 
         log.info("获取 open_id 成功：{}", session.toString());
+    }
+
+    @PostMapping("/getPhoneNumber")
+    public void getPhoneNumber(@RequestBody GetPhoneNumberReq req) throws WxErrorException {
+
+        try {
+            WxMaPhoneNumberInfo phoneNumberInfo = wxMaService.getUserService()
+                                                             .getPhoneNoInfo(req.getCode());
+
+            log.info("{}", phoneNumberInfo);
+            // 打印手机号
+            log.info("用户手机号：{}", phoneNumberInfo.getPhoneNumber());
+        } catch (WxErrorException e) {
+            e.printStackTrace();
+            throw new RuntimeException("获取手机号失败：" + e.getMessage());
+        }
     }
 }
---
+++ web/wx-mini-server/src/main/java/com/samin/wx/model/GetPhoneNumberReq.java
@@ -0,0 +1,9 @@
+package com.samin.wx.model;
+
+import lombok.Data;
+
+@Data
+public class GetPhoneNumberReq {
+
+    private String code;
+}
