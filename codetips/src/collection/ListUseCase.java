a1df30ca64c9b1c042235e8a7f6dfab45f4e3843
SaminZou <822085977@qq.com>
2025-06-04 18:16:02 +0800

    ✨ Feat: enhance code tips

---
+++ codetips/src/collection/ListUseCase.java
@@ -0,0 +1,23 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * 列表用例
+ * <p>
+ * Description: 列表用例
+ * <p>
+ * Created By: Samin Created Date: 2025-06-04
+ */
+public class ListUseCase {
+
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.addAll(list1.stream()
+                          .collect(Collectors.toList()));
+
+        System.out.println(list2);
+    }
+}
