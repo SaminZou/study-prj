2c9f5ec3911deb12ac5d565bbadce031908cf931
SaminZou <822085977@qq.com>
2025-06-26 08:36:48 +0800

    ✨ Feat: enhance auth demo

--- web/demo-module/auth-demo/src/main/java/com/samin/auth/authentication/CustomUserDetailsChecker.java
+++ web/demo-module/auth-demo/src/main/java/com/samin/auth/authentication/CustomUserDetailsChecker.java
@@ -1,5 +1,8 @@
 package com.samin.auth.authentication;
 
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsChecker;
 import org.springframework.stereotype.Component;
@@ -8,7 +11,20 @@ import org.springframework.stereotype.Component;
 public class CustomUserDetailsChecker implements UserDetailsChecker {
 
     @Override
-    public void check(UserDetails toCheck) {
+    public void check(UserDetails userDetails) {
+        if (!userDetails.isAccountNonExpired()) {
+            // 抛出过期异常
+            throw new AccountExpiredException("Account Expired");
+        }
 
+        if (!userDetails.isAccountNonLocked()) {
+            // 抛出锁定异常
+            throw new LockedException("Account Locked");
+        }
+
+        if (!userDetails.isEnabled()) {
+            // 抛出禁用异常
+            throw new DisabledException("Account Disabled");
+        }
     }
 }
