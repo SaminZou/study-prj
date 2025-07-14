8333a7fb73e93389018249a9ffa722ba6db52250
SaminZou <822085977@qq.com>
2025-07-14 08:20:02 +0800

    ✨ Feat: enhance spring use case

--- web/spring-use-case/src/main/java/com/samin/usecase/scheduler/TestScheduler.java
+++ web/spring-use-case/src/main/java/com/samin/usecase/scheduler/TestScheduler.java
@@ -2,6 +2,11 @@ package com.samin.usecase.scheduler;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,6 +16,30 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableScheduling
 public class TestScheduler {
+
+    private final ScheduledExecutorService retryScheduler = Executors.newSingleThreadScheduledExecutor();
+
+    private int retryCount = 0;
+    // minutes
+    private final List<Long> retryDelays = Arrays.asList(1L, 15L, 30L);
+
+    @Scheduled(cron = "0 0 10 * * ?")
+    public void fetchDataTask() {
+        fetchDataAndRetryIfNeeded();
+    }
+
+    private void fetchDataAndRetryIfNeeded() {
+        if (retryCount < retryDelays.size()) {
+            long delay = retryDelays.get(retryCount);
+            retryCount++;
+            System.out.printf("返回空数据，%d 分钟后重试（第 %d 次）...\n", delay, retryCount);
+
+            retryScheduler.schedule(this::fetchDataAndRetryIfNeeded, delay, TimeUnit.MINUTES);
+        } else {
+            System.out.println("已重试三次仍无数据，终止重试。");
+        }
+    }
+
 
     /**
      * 每隔 15 秒运行一次
