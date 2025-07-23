d4d6f4285cc2a1f24a1ba772c84f542a0a3b2f71
SaminZou <822085977@qq.com>
2025-07-23 08:36:11 +0800

    ✨ Feat: enhance demo module

--- web/demo-module/auth-demo/src/test/java/com/samin/auth/service/UserServiceTest.java
+++ web/demo-module/auth-demo/src/test/java/com/samin/auth/service/UserServiceTest.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Slf4j
 @SpringBootTest
@@ -12,8 +13,15 @@ class UserServiceTest {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
+    void getPassword() {
+        log.info("{}", passwordEncoder.encode("123456"));
+    }
+
+    // @Test
     void initUser() {
         // 初始用户
         UserSaveReq user = new UserSaveReq();
