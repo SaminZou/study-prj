eed07153a00fbe6dee7b6a9fd7e4aaf4998f8ecd
SaminZou <822085977@qq.com>
2025-08-23 09:34:14 +0800

    ✨ Feat: enhance sql

---
+++ SQL/MySQL/show_connections.sql
@@ -0,0 +1,4 @@
+SELECT
+    @@max_connections AS '最大连接数',
+        (SELECT VARIABLE_VALUE FROM performance_schema.global_status WHERE VARIABLE_NAME = 'Max_used_connections') AS '历史最大连接数',
+        (SELECT VARIABLE_VALUE FROM performance_schema.global_status WHERE VARIABLE_NAME = 'Threads_connected') AS '当前连接数';
