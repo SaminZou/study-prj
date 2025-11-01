e8d4378ff38810c9ce4aad275ea07e27979f17a6
SaminZou <822085977@qq.com>
2025-11-01 15:59:14 +0800

    ✨ Feat: enhance mqtt producer server

--- web/mqtt-producer-server/src/main/java/com/samin/mqtt/controller/MsgSendController.java
+++ web/mqtt-producer-server/src/main/java/com/samin/mqtt/controller/MsgSendController.java
@@ -1,8 +1,14 @@
 package com.samin.mqtt.controller;
 
+import com.samin.mqtt.service.MsgSendService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/msg")
-public class MsgSendController {}
+@RequiredArgsConstructor
+public class MsgSendController {
+
+    private final MsgSendService msgSendService;
+}
---
+++ web/mqtt-producer-server/src/main/java/com/samin/mqtt/service/MsgSendService.java
@@ -0,0 +1,6 @@
+package com.samin.mqtt.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MsgSendService {}
