badf2b713bddfa26303399801d6d717233babe54
SaminZou <822085977@qq.com>
2025-12-02 16:05:14 +0800

    ✨ Feat: enhance java script

--- javascript/object-attributes-determine.js
+++ javascript/object-attributes-determine.js
@@ -1,19 +1,24 @@
-let obj = { a: 0}
-let obj2 = { a: 1}
+let obj = {a: 0}
+let obj2 = {a: 1}
 
-// 以下两个用法等同
-if (obj.a !== undefined && obj.a !== null && obj.a !== "" && obj.a * 1 > 0){
+// (obj.a) 和 (obj.a !== undefined && obj.a !== null && obj.a !== "" && obj.a * 1 > 0) 用法相同
+if (obj.a !== undefined && obj.a !== null && obj.a !== "" && obj.a * 1 > 0) {
+    // 不输出
     console.log("obj attibutes exist")
 }
 
-if (obj.a){
+if (obj.a) {
+    // 不输出
     console.log("obj attibutes exist")
 }
 
-if (obj2.a !== undefined && obj2.a !== null && obj2.a !== "" && obj2.a * 1 > 0){
+if (obj2.a !== undefined && obj2.a !== null && obj2.a !== "" && obj2.a * 1
+    > 0) {
+    // 输出
     console.log("obj2 attibutes exist")
 }
 
-if (obj2.a){
+if (obj2.a) {
+    // 输出
     console.log("obj2 attibutes exist")
 }
