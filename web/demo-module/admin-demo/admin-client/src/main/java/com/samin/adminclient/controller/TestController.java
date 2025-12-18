aae61a67ce5e38a235e5ff012bdeafd39fed951e
SaminZou <822085977@qq.com>
2025-12-18 16:22:28 +0800

    ✨ Feat: enhance admin demo

--- web/demo-module/admin-demo/admin-client/src/main/java/com/samin/adminclient/controller/TestController.java
+++ web/demo-module/admin-demo/admin-client/src/main/java/com/samin/adminclient/controller/TestController.java
@@ -3,6 +3,9 @@ package com.samin.adminclient.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试控制类
+ */
 @RestController
 public class TestController {
 
