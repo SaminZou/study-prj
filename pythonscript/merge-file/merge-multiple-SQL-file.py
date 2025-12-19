9f957f2f97b6bb9f7323fc923c5f62839153613c
SaminZou <822085977@qq.com>
2025-12-19 08:47:52 +0800

    ✨ Feat: enhance python script

--- pythonscript/merge-file/merge-multiple-SQL-file.py
+++ pythonscript/merge-file/merge-multiple-SQL-file.py
@@ -1,20 +1,20 @@
 import os
 
-# 获取目录中的所有SQL文件
+# 获取目录中的所有 SQL文件
 sql_files_dir = './files'
 output_file_path = './test.sql'
 sql_files = [f for f in os.listdir(sql_files_dir) if f.endswith('.sql')]
 
 # 创建输出文件
 with open(output_file_path, 'w', encoding='UTF-8') as output_file:
-    # 遍历每个SQL文件
+    # 遍历每个 SQL文件
     for sql_file in sql_files:
         # 写入文件名作为注释
         output_file.write('-- File: {}\n'.format(sql_file))
 
-        # 读取SQL文件内容
+        # 读取 SQL文件内容
         with open(os.path.join(sql_files_dir, sql_file), 'r', encoding='UTF-8') as input_file:
-            # 将SQL文件内容写入输出文件
+            # 将 SQL文件内容写入输出文件
             output_file.write(input_file.read())
 
         # 添加分隔行
