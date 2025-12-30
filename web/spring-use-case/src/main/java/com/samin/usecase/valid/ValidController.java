6e1efb72e5a3f71cf08933670e03bf1d911943b3
SaminZou <822085977@qq.com>
2025-12-31 03:39:32 +0800

    ✨ Feat: enhance spring use case

--- web/spring-use-case/src/main/java/com/samin/usecase/valid/ValidController.java
+++ web/spring-use-case/src/main/java/com/samin/usecase/valid/ValidController.java
@@ -6,6 +6,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
@@ -25,6 +26,11 @@ public class ValidController {
 
     @PostMapping("/valid/test")
     public void validTest(@RequestBody @Valid Req req) {
+    }
+
+    @GetMapping("/valid/system/type")
+    public void validTest(@RequestParam(required = false) String systemType) {
+        System.out.println(systemType);
     }
 
     /**
